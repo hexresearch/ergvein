dcee8450a05f87a57052462d1d3e195edb577f6c
NCrashed <ncrashed@gmail.com>
2019-10-02 13:00:59 +0300

    Add assets for android

--- default.nix
+++ default.nix
@@ -15,4 +15,24 @@ in reflex-platform.project ({ pkgs, ... }: {
     ];
   };
   overrides = import ./overrides.nix { inherit reflex-platform; };
+
+  android.ergvein-wallet = {
+    executableName = "ergvein";
+    applicationId = "org.ergvein.wallet";
+    displayName = "Ergvein wallet";
+    resources = ./wallet/static/res;
+    assets = ./wallet/static/assets;
+    iconPath = "@drawable/ic_launcher";
+    version = {
+      code = "1";
+      name = "Alpha";
+    };
+    releaseKey = if release then {
+      storeFile = ./release/release.keystore;
+      storePassword = builtins.readFile ./release/password;
+      keyAlias = "ergvein_releasekey";
+      keyPassword = builtins.readFile ./release/password;
+    } else null;
+  };
+
 })
