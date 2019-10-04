f6c8b4606032b155e7a48160445ddf12af465792
NCrashed <ncrashed@gmail.com>
2019-10-04 17:15:01 +0300

    Add state recovery to mnemonicWidget

--- wallet/src/Ergvein/Wallet/Page/Seed.hs
+++ wallet/src/Ergvein/Wallet/Page/Seed.hs
@@ -11,12 +11,13 @@ import qualified Data.Text as T
 
 mnemonicPage :: MonadFront t m => m ()
 mnemonicPage = container $ do
-  _ <- mnemonicWidget
+  _ <- mnemonicWidget Nothing
   pure ()
 
-mnemonicWidget :: MonadFront t m => m (Event t Text)
-mnemonicWidget = do
-  phrase <- pure $ T.unwords mockSeed
+mnemonicWidget :: MonadFront t m => Maybe Text -> m (Event t Text)
+mnemonicWidget mnemonic = do
+  let generateMnemonic = pure $ T.unwords mockSeed -- TODO: here insert generation of new mnemonic phrase
+  phrase <- maybe generateMnemonic pure mnemonic
   divClass "mnemonic-title" $ h4 $ text "Theese words are your seed phrase"
   colonize 4 (T.words phrase) $ divClass "column mnemonic-word" . text
   divClass "mnemonic-warn" $ h4 $ text "It is the ONLY way to restore access to your wallet. Write it down or you will lost your money forever."
