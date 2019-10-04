f0bf1f2cc33c7d437bde7abaf91d6866e63de413
NCrashed <ncrashed@gmail.com>
2019-10-04 15:16:29 +0300

    Embed fonts in CSS

--- wallet/src/Ergvein/Wallet/Style.hs
+++ wallet/src/Ergvein/Wallet/Style.hs
@@ -5,21 +5,40 @@ module Ergvein.Wallet.Style(
 import Clay
 import Data.ByteString (ByteString)
 import Data.ByteString.Lazy (toStrict)
+import Data.Text (Text)
 import Data.Text.Lazy.Encoding (encodeUtf8)
-import Ergvein.Wallet.Style.TH
+import Ergvein.Wallet.Embed
+import Ergvein.Wallet.Embed.TH
 import Language.Javascript.JSaddle
 import Prelude hiding ((**), rem)
 
-compileFrontendCss :: MonadJSM m => m ByteString
-compileFrontendCss = pure frontendCssBS
+data Resources = Resources {
+  robotoBlackUrl   :: !Text
+, robotoBoldUrl    :: !Text
+, robotoMediumUrl  :: !Text
+, robotoRegularUrl :: !Text
+}
 
-frontendCssBS :: ByteString
-frontendCssBS = let
-  selfcss = toStrict . encodeUtf8 . renderWith compact [] $ frontendCss
+embedResources :: MonadJSM m => m Resources
+embedResources = Resources
+  <$> createObjectURL robotBlack
+  <*> createObjectURL robotoBold
+  <*> createObjectURL robotoMedium
+  <*> createObjectURL robotoRegular
+
+compileFrontendCss :: MonadJSM m => m ByteString
+compileFrontendCss = do
+  r <- embedResources
+  pure $ frontendCssBS r
+
+frontendCssBS :: Resources -> ByteString
+frontendCssBS r = let
+  selfcss = toStrict . encodeUtf8 . renderWith compact [] $ frontendCss r
   in milligramCss <> tooltipCss <> selfcss
 
-frontendCss :: Css
-frontendCss = do
+frontendCss :: Resources -> Css
+frontendCss r = do
+  fontFamilies r
   html ? textAlign center
   body ? do
     color textColor
@@ -33,3 +52,6 @@ majorBackground = rgb 255 255 255
 
 minorBackground :: Color
 minorBackground = rgb 59 78 122
+
+fontFamilies :: Resources -> Css
+fontFamilies Resources{..} = pure ()
