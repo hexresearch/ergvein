4410ccd89908b540f545f159d5d3b21eb54c4b05
NCrashed <ncrashed@gmail.com>
2019-10-08 14:56:24 +0300

    Add new helpers to Util

--- wallet/src/Ergvein/Wallet/Util.hs
+++ wallet/src/Ergvein/Wallet/Util.hs
@@ -1,8 +1,12 @@
 module Ergvein.Wallet.Util(
     widgetHoldDyn
   , updatedWithInit
+  , poke
+  , sampleDyn
+  , check
   ) where
 
+import Control.Monad.Except
 import Reflex.Dom
 
 -- | Same as 'widgetHold' but for dynamic
@@ -16,3 +20,16 @@ updatedWithInit :: PostBuild t m => Dynamic t a -> m (Event t a)
 updatedWithInit da = do
   buildE <- getPostBuild
   pure $ leftmost [updated da, current da `tag` buildE]
+
+-- | Fliped version of `pushAlways`
+poke :: Reflex t => Event t a -> (a -> PushM t b) -> Event t b
+poke = flip pushAlways
+
+-- | Sample dynamic instead of `Behavior`
+sampleDyn :: (MonadSample t m, Reflex t) => Dynamic t a -> m a
+sampleDyn = sample . current
+
+-- | Helper to throw error when predicate is not 'True'
+check :: MonadError a m => a -> Bool -> m ()
+check a False = throwError a
+check _ True = pure ()
