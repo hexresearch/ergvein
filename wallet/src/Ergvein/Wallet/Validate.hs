d888bba966a89ed6e8cf16f4c195510660abcd40
NCrashed <ncrashed@gmail.com>
2019-10-08 15:13:23 +0300

    Define validate helper

--- wallet/src/Ergvein/Wallet/Validate.hs
+++ wallet/src/Ergvein/Wallet/Validate.hs
@@ -1,3 +1,13 @@
 module Ergvein.Wallet.Validate(
-
+    validate
   ) where
+
+import Ergvein.Wallet.Monad
+
+-- | Print in place error message when value is `Left`
+validate :: MonadFront t m => Event t (Either Text a) -> m (Event t a)
+validate e = do
+  widgetHold_ (pure ()) $ fforMaybe e $ \case
+    Left err -> Just $ divClass "validate-error" $ text err
+    _ -> Nothing
+  pure $ fmapMaybe (either (const Nothing) Just) e
