f3c0917658acd1be13967db160a7f7f805542394
NCrashed <ncrashed@gmail.com>
2019-10-08 14:57:21 +0300

    Remove helpers from old place

--- wallet/src/Ergvein/Wallet/Elements.hs
+++ wallet/src/Ergvein/Wallet/Elements.hs
@@ -146,15 +146,3 @@ buttonClass classValD sd = do
         pure [("class", classVal), ("href", "javascript:void(0)")]
   (e, _) <- elDynAttr' "button" classesD  $ dynText sd
   return $ domEvent Click e
-
--- | Same as 'widgetHold' but for dynamic
-widgetHoldDyn :: forall t m a . (DomBuilder t m, MonadHold t m) => Dynamic t (m a) -> m (Dynamic t a)
-widgetHoldDyn maD = do
-  ma <- sample . current $ maD
-  widgetHold ma $ updated maD
-
--- | Same as 'updated', but fires init value with 'getPostBuild'
-updatedWithInit :: PostBuild t m => Dynamic t a -> m (Event t a)
-updatedWithInit da = do
-  buildE <- getPostBuild
-  pure $ leftmost [updated da, current da `tag` buildE]
