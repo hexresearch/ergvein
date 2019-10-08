0ca0a99dc04a72bd1a9a2436bd985df015a8823f
NCrashed <ncrashed@gmail.com>
2019-10-08 18:33:42 +0300

    No password option for widget

--- wallet/src/Ergvein/Wallet/Page/Password.hs
+++ wallet/src/Ergvein/Wallet/Page/Password.hs
@@ -8,6 +8,6 @@ import Ergvein.Wallet.Password
 
 passwordPage :: MonadFront t m => m ()
 passwordPage = container $ do
-  divClass "password-setup-title" $ h4 $ text "Setup of encryption password for your wallet"
-  divClass "password-setup-descr" $ h5 $ text "The password is used every time you perform an operation with your money."
+  divClass "password-setup-title" $ h4 $ text "Setup encryption password for your wallet"
+  divClass "password-setup-descr" $ h5 $ text "The password is used every time you perform an operation with your money. Leave the fields empty to set no password for your wallet."
   void $ setupPassword
--- wallet/src/Ergvein/Wallet/Password.hs
+++ wallet/src/Ergvein/Wallet/Password.hs
@@ -9,9 +9,11 @@ import Ergvein.Wallet.Input
 import Ergvein.Wallet.Monad
 import Ergvein.Wallet.Validate
 
+import qualified Data.Text as T
+
 type Password = Text
 
-setupPassword :: MonadFront t m => m (Event t Password)
+setupPassword :: MonadFront t m => m (Event t (Maybe Password))
 setupPassword = divClass "setup-password" $ form $ fieldset $ do
   p1D <- passwordField "Password"
   p2D <- passwordField "Repeat password"
@@ -20,7 +22,7 @@ setupPassword = divClass "setup-password" $ form $ fieldset $ do
     p1 <- sampleDyn p1D
     p2 <- sampleDyn p2D
     check "Passwords must match!" $ p1 == p2
-    pure p1
+    pure $ if T.null p1 then Nothing else Just p1
 
 -- | Password field with toggleable visibility
 passwordField :: MonadFront t m => Text -> m (Dynamic t Password)
