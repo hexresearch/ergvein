7b5676569d9c742aa12d35c49cabf018c848000b
NCrashed <ncrashed@gmail.com>
2019-10-08 15:21:37 +0300

    Elaborate setupPassword

--- wallet/src/Ergvein/Wallet/Password.hs
+++ wallet/src/Ergvein/Wallet/Password.hs
@@ -3,9 +3,28 @@ module Ergvein.Wallet.Password(
   , setupPassword
   ) where
 
+import Control.Monad.Except
+import Ergvein.Wallet.Elements
 import Ergvein.Wallet.Monad
+import Ergvein.Wallet.Validate
 
 type Password = Text
 
 setupPassword :: MonadFront t m => m (Event t Password)
-setupPassword = pure never
+setupPassword = divClass "setup-password" $ form $ fieldset $ do
+  p1D <- passwordField "Password"
+  p2D <- passwordField "Repeat password"
+  e <- submitButton "Set"
+  validate $ poke e $ const $ runExceptT $ do
+    p1 <- sampleDyn p1D
+    p2 <- sampleDyn p2D
+    check "Passwords must match!" $ p1 == p2
+    pure p1
+
+-- | Password field with toggleable visibility
+passwordField :: MonadFront t m => Text -> m (Dynamic t Password)
+passwordField _ = pure $ pure ""
+
+-- | Form submit button
+submitButton :: MonadFront t m => Text -> m (Event t ())
+submitButton _ = pure never
