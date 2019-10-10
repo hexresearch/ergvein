22ca12750a6b7d49f9f3271cb03b210cd17c4898
NCrashed <ncrashed@gmail.com>
2019-10-10 15:52:07 +0300

    Define WIP MonadRetract #17

--- retractable/src/Reflex/Dom/Retractable/Class.hs
+++ retractable/src/Reflex/Dom/Retractable/Class.hs
@@ -1,3 +1,11 @@
 module Reflex.Dom.Retractable.Class(
-
+    MonadRetract(..)
   ) where
+
+import Reflex (Event)
+
+-- | Defines context of widget that can switch control to next widget
+-- and can be returned back with preserving of state.
+class MonadRetract t m where
+  -- | Switch current widget to the next widget
+  nextWidget :: Event t (m ()) -> m ()
