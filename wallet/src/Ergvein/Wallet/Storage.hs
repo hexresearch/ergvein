d31e623ef9ff76e2df9c3d3a41bd20676099f37e
lemarwin <lemarwin42@gmail.com>
2019-10-30 22:55:58 +0100

    loadStorageFromFile

--- wallet/src/Ergvein/Wallet/Storage.hs
+++ wallet/src/Ergvein/Wallet/Storage.hs
@@ -1,16 +1,22 @@
 module Ergvein.Wallet.Storage
   (
     ErgveinStorage(..)
+  , loadStorageFromFile
   ) where
 
-import Network.Haskoin.Keys
-import Network.Haskoin.Address
-
-import qualified Data.Map.Strict as M
-
+import Control.Monad.IO.Class
+import Data.Text(Text)
 import Ergvein.Aeson
 import Ergvein.Crypto
+import Ergvein.Text
+import Ergvein.Wallet.Native
 import Ergvein.Wallet.Storage.Secure.Data
+import Network.Haskoin.Address
+import Network.Haskoin.Keys
+import qualified Data.Map.Strict as M
+import qualified Data.Text as T
+
+type Password = Text
 
 data ErgveinStorage = ErgveinStorage{
   storageWallet   :: EncryptedWalletData
@@ -18,3 +24,18 @@ data ErgveinStorage = ErgveinStorage{
 }
 
 $(deriveJSON defaultOptions ''ErgveinStorage)
+
+storageFileName :: Text
+storageFileName = "storage"
+
+decryptStorage :: MonadIO m => Password -> Text -> m (Either Text ErgveinStorage)
+decryptStorage pass txt = pure $ text2json txt
+
+loadStorageFromFile :: (MonadIO m, PlatformNatives) => Password -> m (Either Text ErgveinStorage)
+loadStorageFromFile pass = do
+  storageRawLines <- readStoredFile storageFileName
+  case storageRawLines of
+    [] -> do
+      appendStoredFile storageFileName ""
+      pure $ Left $ "Storage file is empty"
+    _ -> decryptStorage pass $ T.concat storageRawLines
