b4efbc0d068794e21a63ba7668f428a3f26f407c
lemarwin <lemarwin42@gmail.com>
2019-11-16 14:31:24 +0100

    close #125

--- wallet/src/Ergvein/Wallet/Client.hs
+++ wallet/src/Ergvein/Wallet/Client.hs
@@ -6,95 +6,59 @@ module Ergvein.Wallet.Client
   , getTxHexView
   , getTxFeeHistogram
   , txBroadcast
+  , ClientError(..)
   ) where
 
 import Control.Concurrent
 import Control.Concurrent.Lifted hiding (threadDelay)
+import Control.Concurrent.STM
+import Control.Concurrent.STM.TVar
 import Control.Monad.IO.Class
-import Control.Monad.Reader
 import Control.Monad.Random
+import Control.Monad.Reader
+import Data.Maybe
+import Network.HTTP.Client (Manager)
 import Reflex.ExternalRef
 import Servant.Client
+
+import Data.IntMap.Strict (IntMap)
+import qualified Data.IntMap.Strict as MI
+import qualified Data.List as L
+import qualified Data.Set as S
 
 import Ergvein.Index.Client
 import Ergvein.Text
 import Ergvein.Wallet.Alert
 import Ergvein.Wallet.Language
 import Ergvein.Wallet.Monad.Front
+import Ergvein.Wallet.Localization.Client
 
-import qualified Data.List as L
-import qualified Data.Set as S
-
-import Network.HTTP.Client (Manager)
-
-getBalance :: MonadFrontBase t m => Event t BalanceRequest -> m (Event t BalanceResponse)
+getBalance :: MonadFrontBase t m => Event t BalanceRequest -> m (Event t (Either ClientError BalanceResponse))
 getBalance = requesterImpl getBalanceEndpoint
 
-getTxHashHistory :: MonadFrontBase t m => Event t TxHashHistoryRequest -> m (Event t TxHashHistoryResponse)
+getTxHashHistory :: MonadFrontBase t m => Event t TxHashHistoryRequest -> m (Event t (Either ClientError TxHashHistoryResponse))
 getTxHashHistory = requesterImpl getTxHashHistoryEndpoint
 
-getTxMerkleProof :: MonadFrontBase t m => Event t TxMerkleProofRequest -> m (Event t TxMerkleProofResponse)
+getTxMerkleProof :: MonadFrontBase t m => Event t TxMerkleProofRequest -> m (Event t (Either ClientError TxMerkleProofResponse))
 getTxMerkleProof = requesterImpl getTxMerkleProofEndpoint
 
-getTxHexView :: MonadFrontBase t m => Event t TxHexViewRequest -> m (Event t TxHexViewResponse)
+getTxHexView :: MonadFrontBase t m => Event t TxHexViewRequest -> m (Event t (Either ClientError TxHexViewResponse))
 getTxHexView = requesterImpl getTxHexViewEndpoint
 
-getTxFeeHistogram :: MonadFrontBase t m => Event t TxFeeHistogramRequest -> m (Event t TxFeeHistogramResponse)
+getTxFeeHistogram :: MonadFrontBase t m => Event t TxFeeHistogramRequest -> m (Event t (Either ClientError TxFeeHistogramResponse))
 getTxFeeHistogram = requesterImpl getTxFeeHistogramEndpoint
 
-txBroadcast :: MonadFrontBase t m => Event t TxBroadcastRequest -> m (Event t TxBroadcastResponse)
+txBroadcast :: MonadFrontBase t m => Event t TxBroadcastRequest -> m (Event t (Either ClientError TxBroadcastResponse))
 txBroadcast = requesterImpl txBroadcastEndpoint
-
-data ClientMessages
-  = CMSLoading Int Int Int
-  | CMSError
-  | CMSEmpty
-  | CMSValidationError
-  | CMSDone
-  | CMSTimeout
-  deriving (Eq)
-
-instance LocalizedPrint ClientMessages where
-  localizedShow l v = case l of
-    English -> case v of
-      CMSLoading i mi ma  -> "Loading: " <> showt i <> " of " <> showt ma <> " (" <> showt mi <> ")"
-      CMSError            -> "A request has failed"
-      CMSEmpty            -> "Results are empty"
-      CMSValidationError  -> "Validation error: inconsistent results"
-      CMSDone             -> "Done!"
-      CMSTimeout          -> "Time out!"
-    Russian -> case v of
-      CMSLoading i mi ma  -> "Запрашиваю. " <> showt i <> " из " <> showt ma <> " (" <> showt mi <> ") ответили."
-      CMSError            -> "Один из запросов не удался"
-      CMSEmpty            -> "Результатов нет"
-      CMSValidationError  -> "Ошибка: противоречивые ответы"
-      CMSDone             -> "Готово!"
-      CMSTimeout          -> "Время вышло!"
 
 instance MonadIO m => HasClientManager (ReaderT Manager m) where
   getClientMaganer = ask
 
-data ResAct a = ResActAdd a | ResActSet [a] | ResActTimeout | ResActNoUrls
-  deriving (Show)
+data ResAct = ResActCheck | ResActTimeout | ResActNoUrls
 
-data ResSignal a = RSEnough [a] | RSTimeout [a] | RSNoUrls [a]
+data ReqSignal a b = ReqFin a | ReqFailTimeout | ReqFailUrls | ReqTimeout b [a] (S.Set BaseUrl)
 
-instance Foldable ResSignal where
-  foldr f b sig = case sig of
-    RSEnough  rs -> foldr f b rs
-    RSTimeout rs -> foldr f b rs
-    RSNoUrls  rs -> foldr f b rs
-
-mergeResStoreMaybe :: Eq a => Int -> ResAct a -> Either [a] (ResSignal a) -> Maybe (Either [a] (ResSignal a))
-mergeResStoreMaybe n act estore = case act of
-  ResActAdd a -> case estore of
-    Left xs' -> Just $ let xs = a:xs' in if length xs >= n then Right (RSEnough xs) else Left xs
-    _ -> Nothing
-  ResActSet xs  -> Just $ Left xs
-  ResActTimeout -> either (Just . Right . RSTimeout) (const Nothing) estore
-  ResActNoUrls  -> either (Just . Right . RSNoUrls) (const Nothing) estore
-
-validateRes :: Eq a => [a] -> Either ClientMessages a
+validateRes :: Eq a => [a] -> Either ClientMessage a
 validateRes rs = case L.nub rs of
   []    -> Left CMSEmpty
   x:[]  -> Right x
@@ -103,103 +67,124 @@ validateRes rs = case L.nub rs of
 -- Implements request logic:
 -- Request from n nodes and check if results are equal.
 -- TODO: Add more sophisticated validation
-requesterImpl :: forall t m e a b . (MonadFrontBase t m, Eq a, Show a)
-  => (BaseUrl -> b -> ReaderT Manager IO (Either e a))   -- Request function
-  -> Event t b                                      -- Request event
-  -> m (Event t a)                                  -- Result
+requesterImpl :: (MonadFrontBase t m, Eq a, Show a, Show b)
+  => (BaseUrl -> b -> ReaderT Manager IO (Either e a))    -- Request function
+  -> Event t b                                            -- Request event
+  -> m (Event t (Either ClientError a))                   -- Result
 requesterImpl endpoint reqE = mdo
-  uss           <- readExternalRef =<< getUrlsRef           -- list of urls
+  uss  <- readExternalRef =<< getUrlsRef
+  let initReqE = ffor reqE (\req -> Just (req, [], uss))
+  drawE <- delay 0.1 $ leftmost [initReqE, redrawE]
+  respE <- fmap (switch . current) $ widgetHold (pure never) $ ffor drawE $ \case
+    Just (req, rs, urls) -> requesterBody urls endpoint rs req
+    _ -> pure never
+  let redrawE = ffor respE $ \case
+        ReqTimeout req [] uss -> Just (req, [], uss)
+        _ -> Nothing
+
+  pure $ fforMaybe respE $ \case
+    ReqFin res        -> Just $ Right res
+    ReqFailTimeout    -> Just $ Left ClientErrTimeOut
+    ReqFailUrls       -> Just $ Left ClientErrNoUrls
+    ReqTimeout _ _ _  -> Nothing
+
+requesterBody :: forall t m e a b . (MonadFrontBase t m, Eq a, Show a)
+  => S.Set BaseUrl
+  -> (BaseUrl -> b -> ReaderT Manager IO (Either e a))
+  -> [a]
+  -> b
+  -> m (Event t (ReqSignal a b))
+requesterBody uss endpoint initRes req = do
+  buildE        <- getPostBuild
   (minN, maxN)  <- readExternalRef =<< getRequiredUrlNumRef -- Required number of confirmations
   dt            <- readExternalRef =<< getRequestTimeoutRef -- Get a timeout
-  let rereqE = fforMaybe outE $ either Just (const Nothing)
-      finE   = fforMaybe outE $ either (const Nothing) Just
-  drawE <- delay 0.1 $ leftmost [reqE, rereqE]
+  timeoutMsgE   <- delay dt buildE                          -- When to show a timeout message
+  timeoutE      <- delay 1 timeoutMsgE                      -- When to actually signal a timeout
+  urls          <- getRandUrls maxN uss                     -- Starting urls. Asked concurrently
+  mng           <- getClientMaganer                         -- Client manger for requests
+  resCountRef   <- liftIO $ newTVarIO $ S.size uss          -- A counter of "visited" urls. Counts down till 0
 
-  timeoutMsgE <- delay dt $ (True, CMSTimeout) <$ drawE
-  timeoutE <- delay 1 timeoutMsgE
-  performEvent $ (liftIO $ putStrLn "TIMEOOOOOUT") <$ timeoutE
+  -- Request all staring urls concurrently and give back events and triggers
+  (ereses, triggers)  <- fmap unzip $ flip traverse (zip [1..] urls) $ \(i, u) -> do
+    (ev, fire) <- newTriggerEvent
+    void $ liftIO $ forkIO $ do
+      liftIO $ threadDelay 10000000
+      fire =<< (pure . pure . (i,)) =<< flip runReaderT mng (endpoint u req)
+    pure (ev, (i,fire))
 
-  outE :: Event t (Either b a) <- fmap (switch . current) $ widgetHold (pure never) $ ffor drawE $ \req -> do
-    toggleLoadingWidget timeoutMsgE
-    urls  <- getRandUrls maxN uss                     -- Initial list of urls to query
-    mng   <- getClientMaganer
-    eresE <- fmap mconcat $ flip traverse urls $ \u -> do
-      (ev, fire) <- newTriggerEvent
-      void $ liftIO $ forkIO $ fire =<< flip runReaderT mng (endpoint u req)
-      pure ev
-    let failE = fforMaybe eresE $ \case
-          Left err -> Just err
-          _ -> Nothing
-        succE = fforMaybe eresE $ \case
-          Right res -> Just res
-          _ -> Nothing
-    rec   -- If a request fails then get a new url and try again
-      let totalFailE = leftmost [failE, extraFailE]
-      -- Collect used urls
-      usedUrlsD <- foldDyn S.insert (S.fromList urls) extraUrlE
-      -- Check if there are still unused urls and pick from them. If all urls were used at least once - shoutout
-      murlE <- performEvent $ ffor (current usedUrlsD `tag` totalFailE) $ \used -> do
-        let diff = S.difference uss used
-        liftIO $ putStrLn $ show $ S.null diff
-        if S.null diff then pure Nothing else fmap Just $ getExtraUrl diff
-      let extraUrlE   = fmapMaybe id murlE
-          noMoreUrlE  = fmapMaybe (maybe (Just ResActNoUrls) (const Nothing)) murlE
-      -- Get extra results, split, feed extraFailE back into totalFailE
-      -- extraResE <- endpointWrapper endpoint extraUrlD rereqE
-      extraResE <- performEventAsync $ ffor extraUrlE $ \url cb ->
-          void $ liftIO $ forkIO $ cb =<< flip runReaderT mng (endpoint url req)
-      let extraFailE = fforMaybe extraResE $ \case
-            Left err -> Just err
-            _ -> Nothing
-          extraSuccE = fforMaybe extraResE $ \case
-            Right res -> Just res
-            _ -> Nothing
+  let eresE :: Event t [(Int, Either e a)]            -- a response event. Carries ids and possble results
+      fires :: IntMap ([(Int, Either e a)] -> IO ())  -- a map of all the triggers
+      failE :: Event t [Int]                          -- falure event. Carries ids of failed requests
+      succE :: Event t [a]                            -- success event. Carries results
+      eresE = mconcat ereses
+      fires = MI.fromList triggers
+      failE = fforMaybe eresE $ \ies -> let
+        ids = flip fmap ies $ \(i,eres) -> either (const $ Just i) (const Nothing) eres
+        in nullToMaybe $ catMaybes ids
+      succE = fforMaybe eresE $ \ies -> let
+        ress = flip fmap ies $ \(i,eres) -> either (const Nothing) Just eres
+        in nullToMaybe $ catMaybes ress
 
-    let storeActE = leftmost [ResActAdd <$> succE, ResActAdd <$> extraSuccE, ResActTimeout <$ timeoutE, noMoreUrlE]
-    storeD <- foldDynMaybe (mergeResStoreMaybe maxN) (Left []) storeActE
-    resE <- handleDangerMsg $ fforMaybe (updated storeD) $ \case
-      Left _ -> Nothing
-      Right act -> case act of
-        RSEnough  rs -> Just $ validateRes rs
-        RSTimeout rs -> if length rs >= minN then Just (validateRes rs) else Nothing
-        RSNoUrls  rs -> if length rs >= minN then Just (validateRes rs) else Nothing
+  -- Whenever a request has ended, bump the number of "used" urls
+  -- If a request has failed, check if there are still unused urls
+  -- If there are none -- send ResActNoUrls
+  noMoreUrlE' <- fmap (fmapMaybe id) $ performEvent $ ffor eresE $ \ies -> do
+    ress <- flip traverse ies $ \(i, eres) -> liftIO $ do
+      c <- atomically $ modifyTVar' resCountRef (flip (-) 1) >> readTVar resCountRef
+      pure $ either (const $ if c <= 1 then Just ResActNoUrls else Nothing) (const Nothing) eres
+    pure $ listToMaybe $ catMaybes ress
+  noMoreUrlE <- delay 0.5 noMoreUrlE'
 
-    -- Handle messages for loading display
-    toggleLoadingWidget $ ffor reqE           $ \_        -> (True , CMSLoading 0 minN maxN)
-    toggleLoadingWidget $ ffor totalFailE     $ \err      -> (True , CMSError)
-    toggleLoadingWidget $ ffor (updated storeD) $ \store  -> let l = either length length store in (True , CMSLoading l minN maxN)
-    toggleLoadingWidget $ ffor resE           $ \_        -> (False, CMSDone)
+  rec
+    usedUrlsD   <- foldDyn (\us acc -> foldr S.insert acc $ snd (unzip us)) (S.fromList urls) $ extraUrlsE
+    extraUrlsE  <- fmap (fmapMaybe id) $ performEvent $ ffor (current usedUrlsD `attach` failE) $ \(used, ies) -> do
+      ress <- foldM (\(diff, acc) i -> if S.null diff then pure (diff, acc) else do
+          u <- getExtraUrl diff
+          pure (S.delete u diff, (i,u):acc)
+          ) (S.difference uss used,[]) ies
+      pure $ nullToMaybe $ snd ress
 
+  -- Request from extra url. i is ised to pick a particular event trigger
+  -- Otherwise, if performEvent returns an event with extra results,
+  -- it could get messy if, while a forked request waits for a response,
+  -- another request fails and fiers extraUrlE again, overriding control of the event
+  -- At least this is a working theory
+  performEvent_ $ ffor extraUrlsE $ \ius -> void $ flip traverse ius $ \(i,url) -> whenJust (MI.lookup i fires) $
+    \fire -> void $ liftIO $ forkIO $ fire =<< (pure . pure . (i,)) =<< flip runReaderT mng (endpoint url req)
 
-    liftIO $ do
-      putStrLn $ show (minN, maxN)
-      putStrLn $ show $ baseUrlPort <$> urls
-    -- performEvent $ ffor extraUrlE $ liftIO . putStrLn . showBaseUrl
-    -- performEvent $ ffor extraFailE $ const $ liftIO (print "FAAAAAIL")
-    performEvent $ ffor noMoreUrlE $ const $ liftIO (print "NO MORE URLS")
-    performEvent $ ffor storeActE $ liftIO . putStrLn . show
-    performEvent $ ffor (updated usedUrlsD) $ liftIO . putStrLn . (++) "UsedD: " . show . fmap baseUrlPort . S.elems
-    performEvent $ ffor murlE $ \case
-      Just _ -> liftIO $ putStrLn "Murl: Just"
-      Nothing -> liftIO $ putStrLn "Murl: Nothing"
-    performEvent $ ffor (updated storeD) $ \case
-      Left rs ->  liftIO $ putStrLn $ "Left: " ++ (show $ length rs)
-      Right act -> liftIO $ putStrLn $ case act of
-        RSEnough  rs  -> "RSEnough: " ++ (show $ length rs)
-        RSTimeout rs  -> "RSTimeout: " ++ (show $ length rs)
-        RSNoUrls  _   -> "RSNoUrls"
+  storeD  <- foldDyn (++) initRes succE
+  checkE  <- delay 0.05 succE
+  rec
+    let actE = gate gateB $ leftmost [ResActTimeout <$ timeoutE, noMoreUrlE, ResActCheck <$ checkE]
+    let preValidE = flip push actE $ \act -> do
+          res   <- sample $ current storeD
+          used  <- sample $ current usedUrlsD
+          let l = length res
+          let diff = S.difference uss used
+          pure $ case act of
+              ResActCheck   -> if l >= maxN then Just (Right res) else Nothing
+              ResActTimeout -> Just $ if l >= minN
+                then Right res
+                else Left $ if S.null diff then ReqFailTimeout else ReqTimeout req res diff
+              ResActNoUrls  -> Just $ if l >= minN then Right res else Left ReqFailUrls
 
-    delay 0.1 $ Right <$> resE
+    resE <- handleDangerMsg (ffor preValidE $ either Right (fmap ReqFin . validateRes))
+    -- When a result has fired -- stop caring about the rest and block all subsequent acts
+    gateB <- fmap current $ holdDyn True $ False <$ resE
 
-  pure finE
+  -- Handle messages for loading display
+  toggleLoadingWidget $ (True , CMSTimeout) <$ timeoutMsgE
+  toggleLoadingWidget $ (True , CMSError)   <$ failE
+  toggleLoadingWidget $ ffor resE $ \case
+    ReqTimeout _ _ _ -> (True, CMSRestarting)
+    _ -> (False, CMSDone)
+  toggleLoadingWidget =<< fmap ((True , CMSLoading 0 minN maxN) <$) getPostBuild
+  toggleLoadingWidget $ ffor (updated storeD) $ \store -> (True , CMSLoading (length store) minN maxN)
 
+  delay 0.1 resE
   where
     getExtraUrl :: MonadIO m => S.Set BaseUrl -> Performable m BaseUrl
-    getExtraUrl uss = do
-      liftIO $ putStrLn $ "DIFF: " ++ show (baseUrlPort <$> S.elems uss)
-      u <- liftIO $ fmap (S.elems uss !!) $ getRandomR (0, length uss - 1)
-      liftIO $ putStrLn $ show $ baseUrlPort u
-      pure u
+    getExtraUrl uss = liftIO $ fmap (S.elems uss !!) $ getRandomR (0, length uss - 1)
 
     getRandUrls :: MonadIO m => Int -> S.Set BaseUrl -> m [BaseUrl]
     getRandUrls n usSet = liftIO $ do
@@ -207,3 +192,13 @@ requesterImpl endpoint reqE = mdo
       let perml = product [1 .. (length uss)] - 1
       i <- getRandomR (0, perml)
       pure $ take n $ L.permutations uss !! i
+
+    nullToMaybe :: [x] -> Maybe [x]
+    nullToMaybe xs = case xs of
+      [] -> Nothing
+      _  -> Just xs
+
+    whenJust :: Monad m' => Maybe v -> (v -> m' ()) -> m' ()
+    whenJust mv f = case mv of
+      Nothing -> pure ()
+      Just v  -> f v
