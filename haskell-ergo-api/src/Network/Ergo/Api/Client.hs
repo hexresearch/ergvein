5366fa11a9668dc59c398199747d8d81b69d199f
Aminion <bonumsignum@gmal.com>
2019-12-09 11:40:55 +0200

    Add http client # 146

--- haskell-ergo-api/src/Network/Ergo/Api/Client.hs
+++ haskell-ergo-api/src/Network/Ergo/Api/Client.hs
@@ -1,2 +1,30 @@
-module Network.Ergo.Api.Client where
- 
+module Network.Ergo.Api.Client (Client (..) , newClient ) where
+
+import           Control.Lens              ((&), (?~))
+import qualified Network.Wreq              as W
+import qualified Network.Wreq.Session      as WS
+
+import qualified Data.Text                 as T
+import qualified Data.Text.Encoding        as TE
+
+-- | Client session data
+data Client = Client {
+  clientUrl     :: String,    -- ^ The JSON RPC url
+  clientOpts    :: W.Options, -- ^ Default HTTP options to use with `wreq` requests
+  clientSession :: WS.Session -- ^ Connection reuse of our HTTP session
+  } deriving ( Show )
+
+-- | Initializes a client and prepares it for making requests against the
+--   Ergo API. Connection reuse is provided, and cleanup of any acquired
+--   resources is handled automatically.
+newClient :: String -> Int -> T.Text -> T.Text -> IO Client 
+newClient host port user pass = do
+  let options :: W.Options
+      options = W.defaults & applyAuth
+      
+      applyAuth = W.auth ?~ W.basicAuth (TE.encodeUtf8 user) (TE.encodeUtf8 pass)
+      
+      generateUrl :: String
+      generateUrl = "http://" ++ host ++ ":" ++ show port
+  session <- WS.newAPISession
+  pure $ Client generateUrl options session
