6f39c488b3c610a714a322a93590abccd64ba59b
Sergey Ivanov <ivanovs-4@yandex.ru>
2019-12-17 20:17:55 +0400

    PoPowProof models with JSON instances #163 (#164)
    * PoPowProof models with JSON instances #163
    
    * Rename fields m -> prefixM, k -> suffixK

--- interfaces/ergo/src/Ergvein/Interfaces/Ergo/PoPowHeader.hs
+++ interfaces/ergo/src/Ergvein/Interfaces/Ergo/PoPowHeader.hs
@@ -4,10 +4,13 @@ ergo/src/main/scala/org/ergoplatform/modifiers/history/PoPowHeader.scala
 -}
 module Ergvein.Interfaces.Ergo.PoPowHeader where
 
-import qualified Data.ByteString as BS
+import Data.Aeson as A
 import Data.Serialize                     as S (Serialize (..), decode, encode, get, put)
 import Data.Serialize.Get                 as S
 import Data.Serialize.Put                 as S
+import qualified Data.ByteString as BS
+
+import Ergvein.Aeson
 
 import Ergvein.Interfaces.Ergo.Header
 import Ergvein.Interfaces.Ergo.Scorex.Util.Package
@@ -17,14 +20,14 @@ import Ergvein.Interfaces.Ergo.Scorex.Util.Serialization.VLQLengthPrefixed
 data PoPowHeader = PoPowHeader {
   header :: !Header
 , interlinks :: ![ModifierId]
-} deriving (Eq)
+} deriving (Eq, Show)
 
 instance Serialize PoPowHeader where
     put obj = do
         -- val headerBytes = obj.header.bytes
         -- w.putInt(headerBytes.length)
         -- w.putBytes(headerBytes)
-        put . VLQLengthPrefixed $ encode $ header obj
+        put . VLQLengthPrefixed $ S.encode $ header obj
         -- w.putInt(obj.interlinks.size)
         -- obj.interlinks.foreach(x => w.putBytes(idToBytes(x)))
         put . VLQLengthPrefixed $ interlinks obj
@@ -32,8 +35,10 @@ instance Serialize PoPowHeader where
         -- val headerSize = r.getInt()
         -- val header = HeaderSerializer.parseBytes(r.getBytes(headerSize))
         VLQLengthPrefixed headerbs <- get
-        header <- either fail pure $ decode headerbs
+        header <- either fail pure $ S.decode headerbs
         -- val linksQty = r.getInt()
         -- val interlinks = (0 until linksQty).map(_ => bytesToId(r.getBytes(32)))
         VLQLengthPrefixed interlinks <- get
         pure PoPowHeader {..}
+
+deriveJSON A.defaultOptions ''PoPowHeader
