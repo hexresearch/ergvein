559c8e0bdef092d7339cdf80a4ef1a5d24154dab
nazgull09 <nazgull09@gmail.com>
2019-12-19 23:42:51 +0100

    clearing root dir

--- reflex-dom-canvas/examples/src/Main.hs
+++
@@ -1,7 +0,0 @@
-module Main where
-
-import CanvasTest3D (mainish)
--- import CanvasTest2D (mainish)
-
-main :: IO ()
-main = mainish
--- reflex-dom-canvas/nix/update.sh
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/env nix-shell
-#! nix-shell -i bash -p nix-prefetch-scripts
-
-nix-prefetch-git https://github.com/reflex-frp/reflex-platform > reflex-platform.json
