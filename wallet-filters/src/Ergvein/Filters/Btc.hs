4e284c2abac4dc9009c9e467fbbac6bef938d931
NCrashed <ncrashed@gmail.com>
2020-01-08 16:56:33 +0300

    Add getSegWitAddr function

--- wallet-filters/src/Ergvein/Filters/Btc.hs
+++ wallet-filters/src/Ergvein/Filters/Btc.hs
@@ -16,25 +16,28 @@ module Ergvein.Filters.Btc
   )
 where
 
-import qualified Data.Attoparsec.Binary as A
-import qualified Data.Attoparsec.ByteString as A
-import           Data.ByteArray.Hash (SipKey(..))
-import           Data.ByteString (ByteString)
-import qualified Data.ByteString as BS
-import qualified Data.ByteString.Lazy as BSL
-import qualified Data.ByteString.Builder as B
-import           Data.Serialize (encode)
+import           Data.ByteArray.Hash            ( SipKey(..) )
+import           Data.ByteString                ( ByteString )
+import           Data.Map.Strict                ( Map )
+import           Data.Maybe
+import           Data.Serialize                 ( encode )
 import           Data.Word
-import           Data.Map.Strict (Map)
-import qualified Data.Map.Strict as M
+import           Ergvein.Filters.GCS
 import           GHC.Generics
+import           Network.Haskoin.Address
+import           Network.Haskoin.Block
 import           Network.Haskoin.Crypto         ( Hash160
                                                 , Hash256
                                                 )
-import           Ergvein.Filters.GCS
-import           Network.Haskoin.Address
-import           Network.Haskoin.Block
+import           Network.Haskoin.Script
 import           Network.Haskoin.Transaction
+
+import qualified Data.Attoparsec.Binary        as A
+import qualified Data.Attoparsec.ByteString    as A
+import qualified Data.ByteString               as BS
+import qualified Data.ByteString.Builder       as B
+import qualified Data.ByteString.Lazy          as BSL
+import qualified Data.Map.Strict               as M
 
 -- | Special wrapper around SegWit address (P2WPKH or P2WSH) to distinct it from other types of addresses.
 data SegWitAddress = SegWitPubkey !Hash160 | SegWitScript !Hash256
@@ -66,14 +69,16 @@ data BtcAddrFilter = BtcAddrFilter {
 
 -- | Encoding filter as simple <length><gcs>
 encodeBtcAddrFilter :: BtcAddrFilter -> ByteString
-encodeBtcAddrFilter BtcAddrFilter{..} = BSL.toStrict . B.toLazyByteString $
-  B.word64BE btcAddrFilterN <> B.byteString (encodeGcs btcAddrFilterGcs)
+encodeBtcAddrFilter BtcAddrFilter {..} =
+  BSL.toStrict . B.toLazyByteString $ B.word64BE btcAddrFilterN <> B.byteString
+    (encodeGcs btcAddrFilterGcs)
 
 -- | Decoding filter from raw bytes
 decodeBtcAddrFilter :: ByteString -> Either String BtcAddrFilter
 decodeBtcAddrFilter = A.parseOnly (parser <* A.endOfInput)
-  where
-    parser = BtcAddrFilter
+ where
+  parser =
+    BtcAddrFilter
       <$> A.anyWord64be
       <*> fmap (decodeGcs btcDefP) A.takeByteString
 
@@ -85,15 +90,28 @@ type InputTxs = Map TxHash Tx
 -- in the given block.
 makeBtcFilter :: InputTxs -> Block -> BtcAddrFilter
 makeBtcFilter txs block = undefined
+ where
+  outputSet =
+    catMaybes $ concatMap (fmap getSegWitAddr . txOut) $ blockTxns block
+
+-- | Extract segwit address from transaction output
+getSegWitAddr :: TxOut -> Maybe SegWitAddress
+getSegWitAddr tout = case decodeOutputBS $ scriptOutput tout of
+  Right (PayWitnessPKHash     h) -> Just $ SegWitPubkey h
+  Right (PayWitnessScriptHash h) -> Just $ SegWitScript h
+  _                              -> Nothing
 
 -- | Siphash key for filter is first 16 bytes of the hash (in standard little-endian representation)
 -- of the block for which the filter is constructed. This ensures the key is deterministic while
 -- still varying from block to block.
 blockSipHash :: Block -> SipKey
-blockSipHash = fromBs . BS.reverse . encode . getBlockHash . headerHash . blockHeader
-  where
-    toWord64 = fst . foldl (\(!acc, !i) b -> (acc + fromIntegral b ^ i, i+1)) (0, 1)
-    fromBs bs = SipKey (toWord64 $ BS.unpack . BS.take 8 $ bs) (toWord64 $ BS.unpack . BS.take 8 . BS.drop 8 $ bs)
+blockSipHash =
+  fromBs . BS.reverse . encode . getBlockHash . headerHash . blockHeader
+ where
+  toWord64 =
+    fst . foldl (\(!acc, !i) b -> (acc + fromIntegral b ^ i, i + 1)) (0, 1)
+  fromBs bs = SipKey (toWord64 $ BS.unpack . BS.take 8 $ bs)
+                     (toWord64 $ BS.unpack . BS.take 8 . BS.drop 8 $ bs)
 
 -- | Check that given address is located in the filter.
 applyBtcFilter :: BtcAddrFilter -> SegWitAddress -> Bool
