b8229c585a52ad5e547066e80577b2220d58cfaa
iqsf <iqsf@ya.ru>
2020-01-17 11:07:11 +0100

    Issue 78: First working settings for language.

--- wallet/src/Ergvein/Wallet/Localization/Settings.hs
+++ wallet/src/Ergvein/Wallet/Localization/Settings.hs
@@ -11,14 +11,20 @@ data SettingsPageStrings =
     STPSTitle
   | STPSButLanguage
   | STPSButPinCode
+  | STPSSelectLanguage
+  | STPSSetsPinCode
 
 instance LocalizedPrint SettingsPageStrings where
   localizedShow l v = case l of
     English -> case v of
-      STPSTitle         -> "Settings"
-      STPSButLanguage   -> "Language"
-      STPSButPinCode    -> "PIN Code"
+      STPSTitle             -> "Settings"
+      STPSButLanguage       -> "Language"
+      STPSButPinCode        -> "PIN Code"
+      STPSSelectLanguage    -> "Select language:"
+      STPSSetsPinCode       -> "Settings for PIN code"
     Russian -> case v of
-      STPSTitle         -> "Настройки"
-      STPSButLanguage   -> "Язык"
-      STPSButPinCode    -> "ПИН код"
+      STPSTitle             -> "Настройки"
+      STPSButLanguage       -> "Язык"
+      STPSButPinCode        -> "ПИН код"
+      STPSSelectLanguage    -> "Выберите язык:"
+      STPSSetsPinCode       -> "Настройки ПИН кода"
--- wallet/src/Ergvein/Wallet/Page/Settings.hs
+++ wallet/src/Ergvein/Wallet/Page/Settings.hs
@@ -2,9 +2,10 @@ module Ergvein.Wallet.Page.Settings(
     settingsPage
   ) where
 
+import qualified Data.Map.Strict as M
+
 import Ergvein.Text
 import Ergvein.Wallet.Localization.Settings
---import Ergvein.Types.Currency
 import Ergvein.Wallet.Elements
 import Ergvein.Wallet.Language
 import Ergvein.Wallet.Menu
@@ -20,9 +21,48 @@ settingsPage = do
   let thisWidget = Just $ pure $ settingsPage
   menuWidget STPSTitle thisWidget
   wrapper True $ do
-    -- h3 $ localizedText $ STPSTitle
     divClass "initial-options grid1" $ do
       goLangE <- fmap (GoLanguage <$) $ outlineButton STPSButLanguage
       goPinE  <- fmap (GoPinCode  <$) $ outlineButton STPSButPinCode
+      let goE = leftmost [goLangE, goPinE]
+      void $ nextWidget $ ffor goE $ \spg -> Retractable {
+          retractableNext = case spg of
+            GoLanguage  -> languagePage
+            GoPinCode   -> pinCodePage
+        , retractablePrev = thisWidget
+        }
+
+languagePage :: MonadFront t m => m ()
+languagePage = do
+  let thisWidget = Just $ pure $ languagePage
+  menuWidget STPSTitle thisWidget
+  wrapper True $ do
+    h3 $ localizedText $ STPSSelectLanguage
+    divClass "initial-options grid1" $ do
+      langD <- getLanguage
+      initKey <- sample . current $ langD
+      let listLangsD = ffor langD $ \l -> M.fromList $ fmap (\v -> (v, localizedShow l v)) allLanguages
+      dp <- dropdown initKey listLangsD def {
+                _dropdownConfig_setValue = updated langD
+--              , _dropdownConfig_attributes = do
+--                  atrs <- _dropdownConfig_attributes def
+--                  pure $ atrs <> [ ("id"   , "id_sel_lang" )
+--                                 , ("class", ""            )
+--                                 , ("style", ""            )
+--                                 ]
+              }
+      let selD = _dropdown_value dp
+      selE <- fmap updated $ holdUniqDyn selD
+      widgetHold (pure ()) $ setLanguage <$> selE
+      pure ()
+    pure ()
+
+pinCodePage :: MonadFront t m => m ()
+pinCodePage = do
+  let thisWidget = Just $ pure $ pinCodePage
+  menuWidget STPSTitle thisWidget
+  wrapper True $ do
+    h3 $ localizedText $ STPSSetsPinCode
+    divClass "initial-options grid1" $ do
       pure ()
     pure ()
