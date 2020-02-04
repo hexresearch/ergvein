3cdc2b8525053be8dd9c07fc83d07428392e20c5
iqsf <iqsf@ya.ru>
2020-02-04 15:22:51 +0100

    Issue 79: Draft about page.

--- wallet/src/Ergvein/Wallet/Style.hs
+++ wallet/src/Ergvein/Wallet/Style.hs
@@ -64,6 +64,7 @@ frontendCss r = do
   passwordCss
   initialPageCss
   balancesPageCss
+  aboutPageCss
   loadingWidgetCss
   alertsCss
   selectCss
@@ -291,6 +292,38 @@ balancesPageCss = do
   ".currency-balance" ? do
     display inlineBlock
     float floatRight
+
+aboutPageCss :: Css
+aboutPageCss = do
+  ".about-wrapper" ? do
+    textAlign center
+  ".about-hr-sep" ? do
+    border solid (px 3) black
+  ".about-line" ? do
+    width $ pct 100
+    maxWidth $ px 500
+    display inlineBlock
+    textAlign center
+  ".about-content" ? do
+    display displayTable
+    marginTop $ px 10
+    fontSize $ pt (if isAndroid then 12 else 18)
+  ".about-content-row" ? do
+    display tableRow
+  ".about-content-cell-label" ? do
+    display tableCell
+    let px' = px 5 in padding px' (px 20) px' px'
+    textAlign $ alignSide sideLeft
+    verticalAlign vAlignBottom
+  ".about-content-cell-value" ? do
+    display tableCell
+    let px' = px 5 in padding px' px' px' px'
+    textAlign $ alignSide sideLeft
+    verticalAlign vAlignBottom
+    width $ pct 1
+  ".about-distrib" ? do
+    paddingTop $ px 45
+    fontSize $ pt (if isAndroid then 12 else 18)
 
 loadingWidgetCss :: Css
 loadingWidgetCss = do
