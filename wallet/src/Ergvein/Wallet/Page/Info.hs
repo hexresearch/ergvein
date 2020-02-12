9351b106defd985f12c8d7c9d97c74ea15c5ab13
iqsf <iqsf@ya.ru>
2020-02-12 10:39:18 +0100

    Issue 81: Draft fix of info page.

--- wallet/src/Ergvein/Wallet/Page/Info.hs
+++ wallet/src/Ergvein/Wallet/Page/Info.hs
@@ -2,7 +2,10 @@ module Ergvein.Wallet.Page.Info(
     infoPage
   ) where
 
+import Ergvein.Text
 import Ergvein.Types.Currency
+import Ergvein.Types.Keys
+import Ergvein.Types.Storage
 import Ergvein.Wallet.Elements
 import Ergvein.Wallet.Id
 import Ergvein.Wallet.Input
@@ -10,21 +13,43 @@ import Ergvein.Wallet.Language
 import Ergvein.Wallet.Localization.Info
 import Ergvein.Wallet.Menu
 import Ergvein.Wallet.Monad
+import Ergvein.Wallet.Settings
 import Ergvein.Wallet.Wrapper
+
+import qualified Data.Map.Strict as M
+import Data.Maybe (fromMaybe)
+import Network.Haskoin.Keys
 
 infoPage :: MonadFront t m => Currency -> m ()
 infoPage cur = do
   let thisWidget = Just $ pure $ infoPage cur
   menuWidget (InfoTitle cur) thisWidget
   wrapper True $ do
-    textLabel NameWallet "my_name_wallet"
+    walName <- getWalletName
+    textLabel NameWallet walName
     vertSpacer
-    textLabel TotalBalance "my_name_wallet"
+
+    settings <- getSettings
+    let setUs = getSettingsUnits settings
+
+    bal <- currencyBalance cur
+    let balD = (\v -> showMoneyUnit v setUs) <$> bal
+    balVal <- sample . current $ balD
+    textLabel TotalBalance balVal
     vertSpacer
-    textLabel ConfirmedBalance "my_name_wallet"
+
+    balCfrm <- currencyBalanceConfirm cur
+    let balCfrmD = (\v -> showMoneyUnit v setUs) <$> balCfrm
+    balCfrmVal <- sample . current $ balCfrmD
+    textLabel ConfirmedBalance balCfrmVal
     vertSpacer
-    textLabel MasterPublicKey "my_name_wallet"
+
+    pks :: PublicKeystore <- getPublicKeystore
+    let masterPKeyMb = (xPubExport (getCurrencyNetwork cur)  . egvXPubKey . egvPubKeyсhain'master) <$> M.lookup cur pks
+    textLabel MasterPublicKey $ fromMaybe "" masterPKeyMb
     pure ()
+    where
+      getSettingsUnits = fromMaybe defUnits . settingsUnits
 
 textLabel :: (MonadFrontBase t m, LocalizedPrint l)
   => l -- ^ Label
@@ -43,3 +68,9 @@ textLabel lbl val = do
 
 vertSpacer :: MonadFrontBase t m => m ()
 vertSpacer = divClass "info-v-spacer" blank
+
+currencyBalance :: MonadFront t m => Currency -> m (Dynamic t Money)
+currencyBalance cur = pure $ pure $ Money cur 1
+
+currencyBalanceConfirm :: MonadFront t m => Currency -> m (Dynamic t Money)
+currencyBalanceConfirm = currencyBalance
