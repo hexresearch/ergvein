fdc7e9efd154cef81f22348608cd1f6347c6f22d
iqsf <iqsf@ya.ru>
2020-03-11 09:09:06 +0100

    Issue 251: Work with android activity. WIP step 2.

---
+++ derivations/android-activity.nix
@@ -0,0 +1,12 @@
+{ mkDerivation, base, data-default, jdk, stdenv }:
+mkDerivation {
+  pname = "android-activity";
+  version = "0.1";
+  src = ../android-activity;
+  /* postUnpack = "sourceRoot+=/android-activity; echo source root reset to $sourceRoot"; */
+  libraryHaskellDepends = [ base data-default ];
+  librarySystemDepends = [ jdk ];
+  homepage = "https://github.com/obsidiansystems/android-activity";
+  description = "Turn regular Haskell programs into Android Activities";
+  license = stdenv.lib.licenses.bsd3;
+}
--- nixpkgs-overlays/default.nix
+++ nixpkgs-overlays/default.nix
@@ -1,5 +1,6 @@
 self: super:
 {
+  android-activity = self.callPackage ./derivations/android-activity.nix {};
   secp256k1 = self.callPackage ../derivations/secp256k1.nix {};
   zlibSys = self.callPackage ../derivations/zlibSys.nix {};
 }
