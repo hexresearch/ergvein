fa94b4a39fc831424707d1990bb315d4561599ec
Anton Gushcha <ncrashed@protonmail.com>
2020-03-16 17:22:24 +0530

    WIP on lmdb backend

--- wallet/src/Ergvein/Wallet/Filters/Btc/Queries.hs
+++ wallet/src/Ergvein/Wallet/Filters/Btc/Queries.hs
@@ -5,28 +5,34 @@ module Ergvein.Wallet.Filters.Btc.Queries(
   , foldFilters
   ) where
 
-import Control.Lens
-import Control.Monad.Haskey
-import Control.Monad.State.Strict
-import Data.BTree.Alloc
+import Control.Monad.IO.Class 
 import Data.Maybe
+import Database.LMDB.Simple
 import Ergvein.Filters.Btc 
 import Ergvein.Wallet.Filters.Btc.Types
 import Network.Haskoin.Block
 
-import qualified Data.BTree.Impure as B
-
-insertFilter :: AllocM m => BlockHeight -> BlockHash -> BtcAddrFilter -> SchemaBtc -> m SchemaBtc
-insertFilter h bh f = schemaBtcFilters %%~ B.insert bh f
-                  >=> schemaBtcHeights %%~ B.insert h bh
-
+insertFilter :: MonadIO m => BlockHeight -> BlockHash -> BtcAddrFilter -> Environment ReadWrite -> m ()
+insertFilter h bh f = liftIO . readWriteTransaction $ do 
+  fdb <- getBtcFiltersDb
+  hdb <- getBtcHeightsDb
+  tdb <- getBtcTotalDb
+  put fdb bh $ Just f 
+  put hdb h $ Just bh 
+  mtotal <- get tdb ()
+  case mtotal of 
+    Just total | total >= h -> put tdb () $ Just h  
+    _ -> pure ()
+   
 getFilter :: AllocReaderM m => BlockHeight -> SchemaBtc -> m (Maybe BtcAddrFilter)
 getFilter k s = do 
-  mh <- B.lookup k (s ^. schemaBtcHeights)
+  fdb <- getBtcFiltersDb
+  hdb <- getBtcHeightsDb
+  mh <- get hdb k
   case mh of 
     Nothing -> pure Nothing 
-    Just h -> B.lookup h (s ^. schemaBtcFilters)
-
+    Just h -> get fdb h
+  
 getFiltersHeight :: AllocReaderM m => SchemaBtc -> m BlockHeight
 getFiltersHeight s = fromMaybe 0 . fmap fst <$> B.lookupMax (s ^. schemaBtcHeights)
 
--- wallet/src/Ergvein/Wallet/Filters/Btc/Types.hs
+++ wallet/src/Ergvein/Wallet/Filters/Btc/Types.hs
@@ -1,42 +1,32 @@
 module Ergvein.Wallet.Filters.Btc.Types(
-    SchemaBtc(..)
-  , emptySchemaBtc
-  , schemaBtcFilters
-  , schemaBtcHeights
+    getBtcFiltersDb
+  , getBtcHeightsDb
+  , getBtcTotalDb
   ) where
 
-import Control.Lens (Lens', lens)
-import Control.Monad.Haskey
-import Control.Monad.Haskey.Haskoin
-import Data.Binary (Binary(..))
-import Data.BTree.Impure (Tree)
-import Data.BTree.Primitives (Value)
-import Data.BTree.Primitives.Key
-import Data.Text (Text)
+import Database.LMDB.Simple
 import Ergvein.Filters.Btc
 import Ergvein.Wallet.Platform
-import GHC.Generics (Generic)
 import Network.Haskoin.Block
 import Network.Haskoin.Crypto
 
 import qualified Data.BTree.Impure as B
 import qualified Data.Serialize as S
 
-data SchemaBtc = SchemaBtc {
-  _schemaBtcFilters   :: Tree BlockHash BtcAddrFilter
-, _schemaBtcHeights   :: Tree BlockHeight BlockHash
-} deriving (Generic, Show)
+filtersDbName :: String 
+filtersDbName = "btcfilters"
 
-instance Binary SchemaBtc
-instance Value SchemaBtc
+heightsDbName :: String 
+heightsDbName = "btcheights"
 
-emptySchemaBtc :: SchemaBtc
-emptySchemaBtc = SchemaBtc B.empty B.empty
+totalDbName :: String 
+totalDbName = "btctotal"
 
-schemaBtcFilters :: Lens' SchemaBtc (Tree BlockHash BtcAddrFilter)
-schemaBtcFilters = lens _schemaBtcFilters $ \s x -> s { _schemaBtcFilters = x }
+getBtcFiltersDb :: Transaction ReadWrite (Database BlockHash BtcAddrFilter)
+getBtcFiltersDb = getDatabase $ Just filtersDbName
 
-schemaBtcHeights :: Lens' SchemaBtc (Tree BlockHeight BlockHash)
-schemaBtcHeights = lens _schemaBtcHeights $ \s x -> s { _schemaBtcHeights = x }
+getBtcHeightsDb :: Transaction ReadWrite (Database BlockHeight BlockHash)
+getBtcHeightsDb = getDatabase $ Just heightsDbName
 
-instance Value BtcAddrFilter
+getBtcTotalDb :: Transaction ReadWrite (Database () BlockHeight)
+getBtcTotalDb = getDatabase $ Just totalDbName
