fb19a93fb61c104ea509106e72f831ab68dd1da0
lemarwin <lemarwin42@gmail.com>
2020-04-08 12:10:44 +0200

    Add new settings

--- wallet/src/Ergvein/Wallet/Settings.hs
+++ wallet/src/Ergvein/Wallet/Settings.hs
@@ -5,9 +5,10 @@ module Ergvein.Wallet.Settings (
   , storeSettings
   ) where
 
-import Control.Lens
+import Control.Lens hiding ((.=))
 import Control.Monad.IO.Class
-import Data.Aeson (withText)
+import Data.Aeson hiding (encodeFile)
+import Data.Maybe
 import Data.Default
 import Data.Text(Text, pack, unpack)
 import Data.Time (NominalDiffTime)
@@ -35,23 +36,55 @@ data Settings = Settings {
   settingsLang              :: Language
 , settingsStoreDir          :: Text
 , settingsConfigPath        :: Text
-, settingsDefUrls           :: [BaseUrl]
-, settingsDefUrlNum         :: (Int, Int) -- ^ First is minimum required answers. Second is sufficient amount of answers from indexers.
-, settingsReqTimeout        :: NominalDiffTime
 , settingsUnits             :: Maybe Units
 , settingsActiveCurrencies  :: ActiveCurrencies
+, settingsReqTimeout        :: NominalDiffTime
+, settingsActiveUrls        :: [BaseUrl]
+, settingsDeactivatedUrls   :: [BaseUrl]
+, settingsPassiveUrls       :: [BaseUrl]
+, settingsReqUrlNum         :: (Int, Int) -- ^ First is minimum required answers. Second is sufficient amount of answers from indexers.
+, settingsActUrlNum         :: Int
 } deriving (Eq, Show)
 
-$(deriveJSON (aesonOptionsStripPrefix "settings") ''Settings)
 
 makeLensesWith humbleFields ''Settings
 
--- | TODO: Implement some checks to see if the configPath folder is ok to write to
-storeSettings :: MonadIO m => Settings -> m ()
-storeSettings s = liftIO $ do
-  let configPath = settingsConfigPath s
-  createDirectoryIfMissing True $ unpack $ T.dropEnd 1 $ fst $ T.breakOnEnd "/" configPath
-  encodeFile (unpack configPath) s
+-- $(deriveJSON (aesonOptionsStripPrefix "settings") ''Settings)
+
+instance FromJSON Settings where
+  parseJSON = withObject "Settings" $ \o -> do
+    settingsLang              <- o .: "lang"
+    settingsStoreDir          <- o .: "storeDir"
+    settingsConfigPath        <- o .: "configPath"
+    settingsUnits             <- o .: "units"
+    settingsActiveCurrencies  <- o .: "activeCurrencies"
+    settingsReqTimeout        <- o .: "reqTimeout"
+    mActiveUrls               <- o .: "activeUrls"
+    mDeactivatedUrls          <- o .: "deactivatedUrls"
+    mPassiveUrls              <- o .: "passiveUrls"
+    settingsReqUrlNum         <- o .:? "reqUrlNum"  .!= defaultIndexersNum
+    settingsActUrlNum         <- o .:? "actUrlNum"  .!= 10
+    let (settingsActiveUrls, settingsDeactivatedUrls, settingsPassiveUrls) =
+          case (mActiveUrls, mDeactivatedUrls, mPassiveUrls) of
+            (Nothing, Nothing, Nothing) -> (defaultIndexers, [], [])
+            (Just [], Just [], Just []) -> (defaultIndexers, [], [])
+            _ -> (fromMaybe [] mActiveUrls, fromMaybe [] mDeactivatedUrls, fromMaybe [] mPassiveUrls)
+    pure Settings{..}
+
+instance ToJSON Settings where
+  toJSON Settings{..} = object [
+      "lang"              .= toJSON settingsLang
+    , "storeDir"          .= toJSON settingsStoreDir
+    , "configPath"        .= toJSON settingsConfigPath
+    , "units"             .= toJSON settingsUnits
+    , "activeCurrencies"  .= toJSON settingsActiveCurrencies
+    , "reqTimeout"        .= toJSON settingsReqTimeout
+    , "activeUrls"        .= toJSON settingsActiveUrls
+    , "deactivatedUrls"   .= toJSON settingsDeactivatedUrls
+    , "passiveUrls"       .= toJSON settingsPassiveUrls
+    , "reqUrlNum"         .= toJSON settingsReqUrlNum
+    , "actUrlNum"         .= toJSON settingsActUrlNum
+   ]
 
 defaultIndexers :: [BaseUrl]
 defaultIndexers = [
@@ -60,8 +93,8 @@ defaultIndexers = [
   , parse "https://ergvein-indexer3.hxr.team"
   , parse "https://ergvein-indexer4.hxr.team"
   ]
-  where 
-    parse = either (error . ("Failed to parse default indexer: " ++) . show) id . parseBaseUrl 
+  where
+    parse = either (error . ("Failed to parse default indexer: " ++) . show) id . parseBaseUrl
 
 defaultIndexersNum :: (Int, Int)
 defaultIndexersNum = (2, 4)
@@ -69,29 +102,32 @@ defaultIndexersNum = (2, 4)
 defaultIndexerTimeout :: NominalDiffTime
 defaultIndexerTimeout = 20
 
-defaultSettings :: FilePath -> Settings 
-defaultSettings home =  
+defaultSettings :: FilePath -> Settings
+defaultSettings home =
   let storePath   = home <> "/store"
       configPath  = home <> "/config.yaml"
   in Settings {
-        settingsLang = English
-      , settingsStoreDir = pack storePath
-      , settingsConfigPath = pack configPath
-      , settingsDefUrls = defaultIndexers
-      , settingsDefUrlNum = defaultIndexersNum
-      , settingsReqTimeout = defaultIndexerTimeout
-      , settingsUnits = Just defUnits
-      , settingsActiveCurrencies = ActiveCurrencies mempty
+        settingsLang              = English
+      , settingsStoreDir          = pack storePath
+      , settingsConfigPath        = pack configPath
+      , settingsUnits             = Just defUnits
+      , settingsActiveCurrencies  = ActiveCurrencies mempty
+      , settingsReqTimeout        = defaultIndexerTimeout
+      , settingsActiveUrls        = defaultIndexers
+      , settingsDeactivatedUrls   = []
+      , settingsPassiveUrls       = []
+      , settingsReqUrlNum         = defaultIndexersNum
+      , settingsActUrlNum         = 10
       }
 
-#ifdef ANDROID 
-mkDefSettings :: MonadIO m => FilePath -> m Settings
-mkDefSettings home = liftIO $ do
-  let cfg = defaultSettings home
-  createDirectoryIfMissing True (unpack $ settingsStoreDir cfg)
-  encodeFile (unpack $ settingsConfigPath cfg) cfg
-  pure cfg
+-- | TODO: Implement some checks to see if the configPath folder is ok to write to
+storeSettings :: MonadIO m => Settings -> m ()
+storeSettings s = liftIO $ do
+  let configPath = settingsConfigPath s
+  createDirectoryIfMissing True $ unpack $ T.dropEnd 1 $ fst $ T.breakOnEnd "/" configPath
+  encodeFile (unpack configPath) s
 
+#ifdef ANDROID
 loadSettings :: (MonadIO m, PlatformNatives) => Maybe FilePath -> m Settings
 loadSettings = const $ liftIO $ do
   mpath <- getFilesDir =<< getHaskellActivity
@@ -100,11 +136,13 @@ loadSettings = const $ liftIO $ do
     Just path -> do
       let configPath = path <> "/config.yaml"
       ex <- doesFileExist configPath
-      sett <- if not ex
-        then mkDefSettings path
-        else either (const $ mkDefSettings path) pure =<< readYamlEither' configPath
-      logWrite $ "Loaded settings: " <> showt sett 
-      pure sett 
+      cfg <- if not ex
+        then pure $ defaultSettings path
+        else fmap (either (const $ defaultSettings path) id) $ readYamlEither' configPath
+      logWrite $ "Loaded settings: " <> showt sett
+      createDirectoryIfMissing True (unpack $ settingsStoreDir cfg)
+      encodeFile (unpack $ settingsConfigPath cfg) cfg
+      pure cfg
 
 #else
 mkDefSettings :: MonadIO m => m Settings
@@ -114,10 +152,7 @@ mkDefSettings = liftIO $ do
   putStrLn $ "Config path: " <> home <> "/.ergvein/config.yaml"
   putStrLn $ "Store  path: " <> home <> "/.ergvein/store"
   putStrLn $ "Language   : English"
-  let cfg = defaultSettings (home <> "/.ergvein") 
-  createDirectoryIfMissing True (unpack $ settingsStoreDir cfg)
-  encodeFile (unpack $ settingsConfigPath cfg) cfg
-  pure cfg
+  pure $ defaultSettings (home <> "/.ergvein")
 
 loadSettings :: MonadIO m => Maybe FilePath -> m Settings
 loadSettings mpath = liftIO $ case mpath of
@@ -129,7 +164,10 @@ loadSettings mpath = liftIO $ case mpath of
     loadSettings $ Just path
   Just path -> do
     ex <- doesFileExist path
-    if not ex
+    cfg <- if not ex
       then mkDefSettings
       else either (const mkDefSettings) pure =<< readYamlEither' path
+    createDirectoryIfMissing True (unpack $ settingsStoreDir cfg)
+    encodeFile (unpack $ settingsConfigPath cfg) cfg
+    pure cfg
 #endif
