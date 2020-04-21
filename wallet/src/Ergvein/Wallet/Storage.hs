66ebf72c83b9f6574d38ea319ab16b3c07e8bb85
fastpok <krutkinvs@gmail.com>
2020-04-21 14:56:33 +0300

    Tx types 314 (#326)
    * Reorganization of wallet storage types
    
    * Moved modules Ergvein.Types.Block and Ergvein.Types.Transaction from index-api package to wallet-types package
    * Renamed PublicKeystore to PublicStorage
    
    * ErgveinStorage -> WalletStorage
    
    * Reorganization of wallet storage types
    
    * Added new type CurrencyPrvStorage containing PrvKeystore
    * EgvPrvKeychain -> PrvKeystore
    * EgvPubKeychain -> PubKeystore
    * "private" and "public" substrings in names are replaced by "prv" and "pub"
    
    * Reorganization of wallet storage types
    
    * Added EgvRootXPubKey to PubStorage
    * PubStorage now contains transactions
    * Added To / From JSON instances for ERGO and BTC transactions
    * Fixed functions to support the new storage format
    
    * Init wallet storage data types doc
    
    * Added scheme
    
    * Update storage.md
    
    * Fix after merge

--- wallet/src/Ergvein/Wallet/Storage.hs
+++ wallet/src/Ergvein/Wallet/Storage.hs
@@ -12,10 +12,10 @@ import Ergvein.Wallet.Alert
 -- | Requests password, runs a callback against decoded wallet and returns the result
 -- The callback is in 'Performable m'. Basically 'MonadIO'
 withWallet :: MonadFront t m
-  => Event t (PrivateStorage -> Performable m a) -- ^ Event with a callback
+  => Event t (PrvStorage -> Performable m a) -- ^ Event with a callback
   -> m (Event t a)                               -- ^ results of applying the callback to the wallet
 withWallet reqE = do
-  eps      <- getEncryptedPrivateStorage
+  eps      <- getEncryptedPrvStorage
   widgD    <- holdDyn Nothing $ Just <$> reqE
-  storageE <- handleDangerMsg . fmap (decryptPrivateStorage eps) =<< requestPasssword (() <$ reqE)
+  storageE <- handleDangerMsg . fmap (decryptPrvStorage eps) =<< requestPasssword (() <$ reqE)
   performEvent $ attachWithMaybe (\mwg wall -> mwg <*> pure wall) (current widgD) storageE
