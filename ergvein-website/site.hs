be70b2baf2996deb4e8236e511cc4b3fb641cfb5
lemarwin <lemarwin42@gmail.com>
2020-05-11 20:40:44 +0200

    Header + footer

--- ergvein-website/site.hs
+++ ergvein-website/site.hs
@@ -50,10 +50,9 @@ main = hakyll $ do
     match "index.html" $ do
         route idRoute
         compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
             let indexCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Home"                `mappend`
+                    constField "title" "Home" `mappend`
+                    constField "homepage" "" `mappend`
                     defaultContext
 
             getResourceBody
