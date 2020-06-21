c9db57774502eed0ce394ab646ed9e787d22639b
Aminion <aminion@protonmail.com>
2020-06-21 10:12:11 +0300

    Split index server app

--- index-server/src/Ergvein/Index/Server/App.hs
+++ index-server/src/Ergvein/Index/Server/App.hs
@@ -3,6 +3,7 @@ module Ergvein.Index.Server.App where
 import Network.Wai
 import Network.Wai.Middleware.Cors
 import Network.Wai.Middleware.Gzip
+import Network.HTTP.Types
 
 import Servant
 import Servant.API.Generic
@@ -13,10 +14,16 @@ import Ergvein.Index.Server.Environment
 import Ergvein.Index.Server.Server.V1
 
 indexServerApp :: ServerEnv -> Application
-indexServerApp e = gzip def . appCors $ serve indexApi $ hoistServer indexApi (runServerM e) $ toServant indexServer
+indexServerApp e = gzip def . mid . appCors $ serve indexApi $ hoistServer indexApi (runServerM e) $ toServant indexServer
     where
         appCors = cors $ const $ Just simpleCorsResourcePolicy 
             { corsRequestHeaders = ["Content-Type"]
             , corsMethods = "PUT" : simpleMethods 
             }
 
+mid :: Middleware
+mid app env sendResponse =sendResponse $ responseLBS status200 [] "Hello World"
+
+app :: Application
+app req respond = let 
+  in (respond $ responseLBS status200 [] "Hello World")
