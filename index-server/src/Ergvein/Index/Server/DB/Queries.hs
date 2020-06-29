1fc240027e2712da28e29b69981501fad93e37d4
Aminion <aminion@protonmail.com>
2020-06-29 21:39:14 +0300

    Use leveldb as persistence (#469)
    * Rework discovery  using only cache @454
    
    * add more queries #454
    
    * Add scanned height entity #454
    
    * Add scanned height queries #454
    
    * Remove postgress #454
    
    * Remove db files  #454
    
    * Fix cache init #454
    
    * Remove unused cfg lines #454
    
    * Refactor naming #454
    
    * Remove unused setting #454
    
    * Update exaples #454
    
    * Add fee thread shutdown #454
    
    * Update docker examples #454

--- index-server/src/Ergvein/Index/Server/DB/Queries.hs
+++ index-server/src/Ergvein/Index/Server/DB/Queries.hs
@@ -1,97 +1,143 @@
 module Ergvein.Index.Server.DB.Queries where
 
-import Conduit
-import Control.Concurrent
-import Control.Monad
-import Control.Monad.IO.Class
-import Control.Monad.Reader
+import Control.Lens
+import Data.Default
+import Data.Flat
+import Data.Maybe
+import Database.LevelDB
+import Database.LevelDB.Iterator
+import Control.Monad.Logger
 import Conversion
-import Data.Proxy
 import Data.Time.Clock
-import Data.Word
-import Database.Esqueleto
-import Database.Esqueleto.Pagination
-import Safe
+import Control.Monad.IO.Class
+import Ergvein.Index.Server.Dependencies
 
-import Ergvein.Index.Server.BlockchainScanning.Types
-import Ergvein.Index.Server.DB.Conversions
 import Ergvein.Index.Server.DB.Monad
 import Ergvein.Index.Server.DB.Schema
+import Ergvein.Types.Transaction
+import Ergvein.Index.Server.BlockchainScanning.Types
+import Ergvein.Index.Server.DB.Conversions
 import Ergvein.Index.Server.PeerDiscovery.Types
+import Ergvein.Types.Block
 import Ergvein.Types.Currency
 import Ergvein.Types.Transaction
 
-import qualified Data.Conduit.Internal as DCI
-import qualified Data.Conduit.List as CL
-import qualified Database.Persist as DT
+import qualified Data.Text as T
+import qualified Data.ByteString as BS
+import qualified Data.Serialize as S
+import qualified Data.Map.Strict as Map
+import qualified Database.LevelDB as LDB
+import qualified Database.LevelDB.Streaming as LDBStreaming
 
-pageLoadSize :: PageSize
-pageLoadSize = PageSize 65536
+safeEntrySlice :: (MonadLDB m , Ord k, S.Serialize k, Flat v) => BS.ByteString -> k -> m [(k,v)]
+safeEntrySlice startKey endKey = do
+  db <- getDb
+  iterator <- createIter db def
+  slice <- LDBStreaming.toList $ LDBStreaming.entrySlice iterator range LDBStreaming.Asc
+  pure $ over _2 unflatExact  <$> over _1 (decodeExact . unPrefixedKey) <$> slice
+  where
+    range = LDBStreaming.KeyRange startKey comparison
+    comparison key = case S.decode $ unPrefixedKey key of
+      Right parsedKey -> compare parsedKey endKey
+      _ -> GT
 
-pagedEntitiesStream ::(PersistRecordBackend record backend, PersistQueryRead backend, PersistUniqueRead backend,
-                      BackendCompatible SqlBackend backend, BackendCompatible SqlBackend (BaseBackend backend),
-                      Ord typ, PersistField typ, MonadIO m) 
-                      => EntityField record typ -> ConduitT a [Entity record] (ReaderT backend m) ()
-pagedEntitiesStream entityField = let
-  pagedStream = streamEntities emptyQuery entityField pageLoadSize Ascend (Range Nothing Nothing)
-  in pagedStream .| (CL.chunksOf $ unPageSize pageLoadSize)
+getParsed :: (Flat v, MonadLDB m) => BS.ByteString -> m (Maybe v)
+getParsed key = do
+  db <- getDb
+  maybeResult <- get db def key
+  let maybeParsedResult = unflatExact <$> maybeResult
+  pure maybeParsedResult
 
-getScannedHeight :: MonadIO m => Currency -> QueryT m (Maybe (Entity ScannedHeightRec))
-getScannedHeight currency = fmap headMay $ select $ from $ \scannedHeight -> do
-  where_ (scannedHeight ^. ScannedHeightRecCurrency ==. val currency)
-  pure scannedHeight
+getParsedExact :: (Flat v, MonadLDB m, MonadLogger m) => BS.ByteString -> m v
+getParsedExact key = do
+  db <- getDb
+  maybeResult <- get db def key
+  case maybeResult of
+    Just result -> pure $ unflatExact result
+    Nothing -> do
+      currentTime <- liftIO getCurrentTime
+      logErrorN $ "[Db read miss][getParsedExact] Entity with key " <> (T.pack $ show key) <> " not found at time:" <> (T.pack $ show currentTime)
+      error $ "getParsedExact: not found" ++ show key
 
-upsertScannedHeight :: MonadIO m => Currency -> Word64 -> QueryT m (Entity ScannedHeightRec)
-upsertScannedHeight currency h = upsert (ScannedHeightRec currency h) [ScannedHeightRecHeight DT.=. h]
+getManyParsedExact :: (MonadLDB m, MonadLogger m, Flat v) => [BS.ByteString] -> m [v]
+getManyParsedExact keys = do
+  db <- getDb
+  result <- mapM getParsedExact keys
+  pure result
 
-addNewPeers :: MonadIO m => [NewPeer] -> QueryT m ()
-addNewPeers newPeers = do
+putItems :: (Flat v) => (a -> BS.ByteString) -> (a -> v) -> [a] -> LDB.WriteBatch
+putItems keySelector valueSelector items = putI <$> items
+  where putI item = LDB.Put (keySelector item) $ flat $ valueSelector item
+
+updateTxSpends  :: (MonadLDB m, MonadLogger m) => [TxHash] -> [TxInfo] -> m ()
+updateTxSpends spentTxsHash newTxInfos = do
+  db <- getDb
+  write db def $ putItems (txRecKey . txHash) (convert @_ @TxRec) newTxInfos
+  info <- getManyParsedExact @_ @TxRec  $ txRecKey <$> Map.keys outSpendsAmountByTx 
+  write db def $ infoUpdate <$> info
+  where
+    outSpendsAmountByTx = Map.fromListWith (+) $ (,1) <$> spentTxsHash
+    infoUpdate info = let 
+      outputsLeft = txRecUnspentOutputsCount info - outSpendsAmountByTx Map.! (txRecHash info)
+      in if outputsLeft == 0 then 
+          LDB.Del $ txRecKey $ txRecHash info
+         else
+          LDB.Put (txRecKey $ txRecHash info) (flat $ info { txRecUnspentOutputsCount = outputsLeft })
+
+getKnownPeers :: (MonadLDB m, MonadLogger m, HasDiscoveryRequisites m) => Bool -> m [String]
+getKnownPeers onlySecured = do
+  db <- getDb
+  knownPeers <- getParsedExact knownPeersRecKey
   currentTime <- liftIO getCurrentTime
-  forM_ newPeers (insert_ . convert @_ @DiscoveredPeerRec . (currentTime, ))
-
-refreshPeerValidationTime :: MonadIO m => [DiscoveredPeerRecId] -> QueryT m ()
-refreshPeerValidationTime peerIds = do
-  currentTime <- liftIO getCurrentTime
-  update $ \peer -> do 
-    where_ $ peer ^. DiscoveredPeerRecId `in_` valList peerIds
-    set peer [DiscoveredPeerRecLastValidatedAt =. val currentTime]
-
-deleteExpiredPeers :: MonadIO m => [DiscoveredPeerRecId] -> QueryT m ()
-deleteExpiredPeers peerIds =
-  delete $ from $ \peer -> 
-    where_ $ peer ^. DiscoveredPeerRecId `in_` valList peerIds
-
-getDiscoveredFilteredPeers :: MonadIO m => Bool -> NominalDiffTime -> QueryT m [Peer]
-getDiscoveredFilteredPeers onlySecured actualizationDelay = do
-  currentTime <- liftIO getCurrentTime
+  actualizationDelay <- (/1000000) . fromIntegral . descReqActualizationDelay <$> getDiscoveryRequisites
   let validDate = (-actualizationDelay) `addUTCTime` currentTime
-  result <- select $ from $ \peer -> do
-    when onlySecured $
-      where_ $ peer ^. DiscoveredPeerRecIsSecureConn ==. val onlySecured 
-    where_ $ peer ^. DiscoveredPeerRecLastValidatedAt >=. val validDate
-    pure peer
-  pure $ convert @(Entity DiscoveredPeerRec) <$> result
+  pure $ T.unpack . knownPeerRecUrl <$>
+    if onlySecured then filter knownPeerRecIsSecureConn knownPeers else knownPeers
 
-getDiscoveredPeers :: MonadIO m => QueryT m [Peer]
-getDiscoveredPeers = fmap (convert @(Entity DiscoveredPeerRec)) <$> (select $ from pure)
+getKnownPeersList :: (MonadLDB m, MonadLogger m) => m [Peer]
+getKnownPeersList = do
+  db <- getDb
+  peers <- getParsedExact @[KnownPeerRecItem] knownPeersRecKey
+  pure $ convert <$> peers
 
-insertBlock  :: MonadIO m  => BlockMetaInfo -> QueryT m (Key BlockMetaRec)
-insertBlock block = insert $ convert block
+setKnownPeersList :: (MonadLDB m, MonadLogger m) => [Peer] -> m ()
+setKnownPeersList peers = do
+  db <- getDb
+  put db def knownPeersRecKey $ flat $ convert @_ @KnownPeerRecItem <$> peers
 
-isNonePeersDiscovered :: MonadIO m  => QueryT m Bool
-isNonePeersDiscovered = (0 == ) <$> rowsCount (Proxy :: Proxy DiscoveredPeerRec)
+addKnownPeers :: (MonadLDB m, MonadLogger m) => [Peer] -> m ()
+addKnownPeers peers = do
+  db <- getDb
+  let mapped = convert @_ @KnownPeerRecItem <$> peers
+  stored <- getParsedExact @[KnownPeerRecItem] knownPeersRecKey
+  put db def knownPeersRecKey $ flat $ mapped ++ stored
 
-rowsCount :: forall record m . (BackendCompatible SqlBackend (PersistEntityBackend record),
-                                PersistEntity record, MonadIO m)
-                                 => Proxy record -> QueryT m Word64
-rowsCount _ = do 
-  result <- select $ from (\(_ :: SqlExpr (Entity record)) -> pure $ countRows)
-  pure $ unValue $ head $ result
+getScannedHeight :: (MonadLDB m, MonadLogger m) => Currency -> m (Maybe BlockHeight)
+getScannedHeight currency = do
+  db <- getDb
+  stored <- getParsed $ scannedHeightTxKey currency
+  pure $ scannedHeightRecHeight <$> stored
 
-chunksCount :: forall record m . (BackendCompatible SqlBackend (PersistEntityBackend record),
-                                PersistEntity record, MonadIO m)
-                                 => Proxy record -> QueryT m Word64
-chunksCount _ = do 
-  rCount <- rowsCount (Proxy :: Proxy record)
-  let cCount = ceiling $ fromIntegral rCount / (fromIntegral $ unPageSize pageLoadSize)
-  pure cCount
+setScannedHeight :: (MonadLDB m, MonadLogger m) => Currency -> BlockHeight -> m ()
+setScannedHeight currency height = do
+  db <- getDb
+  put db def (scannedHeightTxKey currency) $ flat $ ScannedHeightRec height
+
+initDb :: DB -> IO ()
+initDb db = do
+  put db def knownPeersRecKey $ flat $ convert @Peer @KnownPeerRecItem <$> []
+
+addBlockInfo :: (MonadLDB m, MonadLogger m) => BlockInfo -> m ()
+addBlockInfo update = do
+  db <- getDb
+  updateTxSpends (spentTxsHash update) $ blockContentTxInfos update
+  addBlockMetaInfos [blockInfoMeta update]
+  setScannedHeight (blockMetaCurrency $ blockInfoMeta update) (blockMetaBlockHeight $ blockInfoMeta update)
+
+addBlockMetaInfos :: (MonadLDB m, MonadLogger m) => [BlockMetaInfo] -> m ()
+addBlockMetaInfos infos = do
+  db <- getDb
+  write db def $ putItems keySelector valueSelector infos
+  where
+    keySelector   info = metaRecKey (blockMetaCurrency info, blockMetaBlockHeight info)
+    valueSelector info = BlockMetaRec (blockMetaHeaderHashHexView info) (blockMetaAddressFilterHexView info)
