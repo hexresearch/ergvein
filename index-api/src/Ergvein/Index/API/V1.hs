ab7d161c1ad92942fe6979eb18fa2820c6d4595f
Aminion <aminion@protonmail.com>
2020-07-02 15:36:58 +0300

    Add ping endpoint #264

--- index-api/src/Ergvein/Index/API/V1.hs
+++ index-api/src/Ergvein/Index/API/V1.hs
@@ -7,12 +7,15 @@ import Ergvein.Types.Currency
 
 type Body a = ReqBody '[JSON] a
 type PostResp a = Post '[JSON] a
+type GetResp a = Get '[JSON] a
 
 type IndexGetHeight         = "height"    :> Body HeightRequest :> PostResp HeightResponse
 
 type IndexGetBlockFilters   = "filters"   :> Body BlockFiltersRequest :> PostResp BlockFiltersResponse
 
-type IndexGetInfo           = "info"      :> PostResp InfoResponse
+type IndexPing              = "ping"      :> GetResp ()
+
+type IndexGetInfo           = "info"      :> GetResp InfoResponse
 
 type IndexIntroducePeer     = "introducePeer"   :> Body IntroducePeerReq :> PostResp IntroducePeerResp
 
@@ -24,6 +27,7 @@ data IndexApi route = IndexApi
     { indexGetHeight         :: route :- IndexGetHeight
     , indexGetBlockFilters   :: route :- IndexGetBlockFilters
     , indexGetInfo           :: route :- IndexGetInfo
+    , indexPing              :: route :- IndexPing
     , indexIntroducePeer     :: route :- IndexIntroducePeer
     , indexKnownPeers        :: route :- IndexKnownPeers
     , indexGetFees           :: route :- IndexGetFees
