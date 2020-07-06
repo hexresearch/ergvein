301b350b1f543200e2527b5190d941cdeebb71a1
Aminion <aminion@protonmail.com>
2020-07-06 20:39:23 +0300

    Add version builders #494

--- index-api/src/Ergvein/Index/API/BinaryTypes.hs
+++ index-api/src/Ergvein/Index/API/BinaryTypes.hs
@@ -2,7 +2,8 @@ module Ergvein.Index.API.BinaryTypes where
 
 import Data.Word
 import Data.ByteString
-import Data.Vector.Unboxed
+import qualified Data.Vector.Unboxed as VU
+import qualified Data.Vector as V
 import Data.ByteString.Builder
 import Foreign.Storable
 
@@ -23,47 +24,69 @@ data MessageType = Version
                  | Pong
 
 
-codeBTC :: ByteString
+codeBTC    :: ByteString
 codeBTC    = "BTC     " --Bitcoin
-codeTBTC :: ByteString
+codeTBTC   :: ByteString
 codeTBTC   = "TBTC    " --Testnet Bitcoin
-codeERGO :: ByteString
+codeERGO   :: ByteString
 codeERGO   = "ERGO    " --Ergo
-codeTERGO :: ByteString
+codeTERGO  :: ByteString
 codeTERGO  = "TERGO   " --Testnet Ergo
-codeUSDTO :: ByteString
+codeUSDTO  :: ByteString
 codeUSDTO  = "USDTO   " --Omni USDT
 codeTUSDTO :: ByteString
 codeTUSDTO = "TUSDTO  " --Testnet Omni USDT
-codeLT :: ByteString
+codeLT     :: ByteString
 codeLT     = "LT      " --Litecoin
-codeTLT :: ByteString
+codeTLT    :: ByteString
 codeTLT    = "TLT     " --Testnet Litecoin
-codeZEC :: ByteString
+codeZEC    :: ByteString
 codeZEC    = "ZEC     " --ZCash
-codeTZEC :: ByteString
+codeTZEC   :: ByteString
 codeTZEC   = "TZEC    " --Testnet ZCash
-codeCPR :: ByteString
+codeCPR    :: ByteString
 codeCPR    = "CPR     " --Cypra token
-codeTCPR :: ByteString
+codeTCPR   :: ByteString
 codeTCPR   = "TCPR    " --Testnet Cypra
 
-verACKMsg :: Builder
-verACKMsg = word32BE msgType 
-         <> word32BE 0
+word32sizeOf :: (Storable a) => a -> Word32
+word32sizeOf = fromIntegral . sizeOf
+
+scanBlock :: Word32 -> Word32 -> Word64 -> Word64 -> Builder
+scanBlock currency version scanHeight height = word32BE currency
+                                            <> word32BE version
+                                            <> word64BE scanHeight
+                                            <> word64BE height
+
+verMsg :: Word32 ->  Word64 -> Word64 -> V.Vector Builder -> Builder
+verMsg version time nonce scanBlocks = 
+  msg msgType msgSize $ word32BE version
+                     <> word64BE time
+                     <> word64BE nonce
+                     <> word32BE currenciesAmount <> V.foldl (<>) mempty scanBlocks
   where
     msgType = 0
+    scanBlockSize = 24
+    currenciesAmount = fromIntegral $ V.length scanBlocks
+    msgSize = word32sizeOf time + word32sizeOf nonce + currenciesAmount * scanBlockSize
+
+verACKMsg :: Builder
+verACKMsg = msg msgType msgSize mempty
+  where
+    msgType = 1
+    msgSize = 0
 
 pingMsg :: Word64 -> Builder
-pingMsg nonce = word32BE msgType 
-             <> word32BE 4 
-             <> word64BE nonce
+pingMsg nonce = msg msgType msgSize $ word64BE nonce
   where
-    msgType = 11
+    msgType = word32sizeOf nonce
+    msgSize = 4
 
 pongMsg :: Word64 -> Builder
-pongMsg nonce = word32BE msgType 
-             <> word32BE 4 
-             <> word64BE nonce
+pongMsg nonce = msg msgType msgSize $ word64BE nonce
   where
     msgType = 12
+    msgSize = word32sizeOf nonce
+
+msg :: Word32 -> Word32 -> Builder -> Builder
+msg msgType msgLength payload = word32BE msgType <> word32BE msgLength <> payload 
