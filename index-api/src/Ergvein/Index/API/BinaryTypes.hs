a31ff61dc64ee94d47dc0e56a1b0bb98e774ad70
Aminion <aminion@protonmail.com>
2020-07-06 18:25:14 +0300

    Add ping/pong builders #494

--- index-api/src/Ergvein/Index/API/BinaryTypes.hs
+++ index-api/src/Ergvein/Index/API/BinaryTypes.hs
@@ -1,6 +1,12 @@
 module Ergvein.Index.API.BinaryTypes where
 
 import Data.Word
+import Data.ByteString
+import Data.Vector.Unboxed
+import Data.ByteString.Builder
+import Foreign.Storable
+
+import qualified Data.ByteString.Lazy               as L
 
 data MessageType = Version
                  | VersionACK
@@ -16,22 +22,48 @@ data MessageType = Version
                  | Ping
                  | Pong
 
-data Message = Message
-  { messageMessageType :: MessageType
-  , messageLength      :: Word32
-  , messagePayload     :: [Word]
-  }
 
-data ScanBlocks = ScanBlocks
-  { scanBlocksCurrency     :: Word
-  , scanBlocksVersion      :: Word32
-  , scanBlocksScanHeight   :: Word64
-  , scanBlocksActualHeight :: Word64
-  }
+codeBTC :: ByteString
+codeBTC    = "BTC     " --Bitcoin
+codeTBTC :: ByteString
+codeTBTC   = "TBTC    " --Testnet Bitcoin
+codeERGO :: ByteString
+codeERGO   = "ERGO    " --Ergo
+codeTERGO :: ByteString
+codeTERGO  = "TERGO   " --Testnet Ergo
+codeUSDTO :: ByteString
+codeUSDTO  = "USDTO   " --Omni USDT
+codeTUSDTO :: ByteString
+codeTUSDTO = "TUSDTO  " --Testnet Omni USDT
+codeLT :: ByteString
+codeLT     = "LT      " --Litecoin
+codeTLT :: ByteString
+codeTLT    = "TLT     " --Testnet Litecoin
+codeZEC :: ByteString
+codeZEC    = "ZEC     " --ZCash
+codeTZEC :: ByteString
+codeTZEC   = "TZEC    " --Testnet ZCash
+codeCPR :: ByteString
+codeCPR    = "CPR     " --Cypra token
+codeTCPR :: ByteString
+codeTCPR   = "TCPR    " --Testnet Cypra
 
-data VersionMessage = VersionMessage
-  { versionMsgVersionNumber :: Word
-  , versionMsgTime          :: Word64
-  , versionMsgNonce         :: Word64
-  , versionMsgCurrencies    :: Word32
-  }
+verACKMsg :: Builder
+verACKMsg = word32BE msgType 
+         <> word32BE 0
+  where
+    msgType = 0
+
+pingMsg :: Word64 -> Builder
+pingMsg nonce = word32BE msgType 
+             <> word32BE 4 
+             <> word64BE nonce
+  where
+    msgType = 11
+
+pongMsg :: Word64 -> Builder
+pongMsg nonce = word32BE msgType 
+             <> word32BE 4 
+             <> word64BE nonce
+  where
+    msgType = 12
