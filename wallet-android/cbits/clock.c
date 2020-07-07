8a7b466936eefc8b35cffbd78a6c8b2b16602647
fastpok <krutkinvs@gmail.com>
2020-07-07 13:11:21 +0300

    Added function to get time zone

--- wallet-android/cbits/clock.c
+++ wallet-android/cbits/clock.c
@@ -15,17 +15,45 @@ jint android_timezone_offset() {
   assert(clockClass);
   __android_log_write(ANDROID_LOG_DEBUG, "android_timezone_offset", "got Clock class");
 
-  jmethodID getCurrentTimezoneOffset = (*env)->GetStaticMethodID(env, clockClass, "getCurrentTimezoneOffset", "()I");
-  assert(getCurrentTimezoneOffset);
-  __android_log_write(ANDROID_LOG_DEBUG, "android_timezone_offset", "got method getCurrentTimezoneOffset");
+  jmethodID getCurrentTimeZoneOffset = (*env)->GetStaticMethodID(env, clockClass, "getCurrentTimeZoneOffset", "()I");
+  assert(getCurrentTimeZoneOffset);
+  __android_log_write(ANDROID_LOG_DEBUG, "android_timezone_offset", "got method getCurrentTimeZoneOffset");
 
-  jint resInt = (*env)->CallStaticIntMethod(env, clockClass, getCurrentTimezoneOffset);
+  jint resInt = (*env)->CallStaticIntMethod(env, clockClass, getCurrentTimeZoneOffset);
   if((*env)->ExceptionOccurred(env)) {
-    __android_log_write(ANDROID_LOG_DEBUG, "android_timezone_offset", "Failed to call getCurrentTimezoneOffset");
+    __android_log_write(ANDROID_LOG_DEBUG, "android_timezone_offset", "Failed to call getCurrentTimeZoneOffset");
     (*env)->ExceptionDescribe(env);
   }
   assert(resInt);
-  __android_log_write(ANDROID_LOG_DEBUG, "android_timezone_offset", "called getCurrentTimezoneOffset method");
+  __android_log_write(ANDROID_LOG_DEBUG, "android_timezone_offset", "called getCurrentTimeZoneOffset method");
 
   return resInt;
+}
+
+const char* android_timezone_id() {
+  JNIEnv *env;
+  jint attachResult = (*HaskellActivity_jvm)->AttachCurrentThread(HaskellActivity_jvm, (void **)&env, NULL);
+  assert(attachResult == JNI_OK);
+  __android_log_write(ANDROID_LOG_DEBUG, "android_timezone_id", "attached to jvm");
+
+  jclass clockClass = (*env)->FindClass(env, "org/ergvein/Clock");
+  assert(clockClass);
+  __android_log_write(ANDROID_LOG_DEBUG, "android_timezone_id", "got Clock class");
+
+  jmethodID getCurrentTimeZoneId = (*env)->GetStaticMethodID(env, clockClass, "getCurrentTimeZoneId", "()Ljava/lang/String;");
+  assert(getCurrentTimeZoneId);
+  __android_log_write(ANDROID_LOG_DEBUG, "android_timezone_id", "got method getCurrentTimeZoneId");
+
+  jobject resStrObj = (*env)->CallStaticObjectMethod(env, clockClass, getCurrentTimeZoneId);
+  if((*env)->ExceptionOccurred(env)) {
+    __android_log_write(ANDROID_LOG_DEBUG, "android_timezone_id", "Failed to call getCurrentTimeZoneId");
+    (*env)->ExceptionDescribe(env);
+  }
+  assert(resStrObj);
+  __android_log_write(ANDROID_LOG_DEBUG, "android_timezone_id", "called getCurrentTimeZoneId method");
+
+  const char* resStr = (*env)->GetStringUTFChars(env, resStrObj, NULL);
+  assert(resStr);
+
+  return resStr;
 }
