c80ba2be28b9041008660909892fc8c326b09836
Aminion <aminion@protonmail.com>
2020-07-07 10:29:23 +0300

    Add currencyTag codes #494

--- index-api/src/Ergvein/Index/API/BinaryTypes.hs
+++ index-api/src/Ergvein/Index/API/BinaryTypes.hs
@@ -23,37 +23,43 @@ data MessageType = Version
                  | Ping
                  | Pong
 
+data CurrencyCode = BTC
+                  | TBTC
+                  | ERGO
+                  | TERGO
+                  | USDTO
+                  | TUSDTO
+                  | LTC
+                  | TLTC
+                  | ZEC
+                  | TZEC
+                  | CPR
+                  | TCPR
+                  | DASH
+                  | TDASH
 
-codeBTC    :: ByteString
-codeBTC    = "BTC     " --Bitcoin
-codeTBTC   :: ByteString
-codeTBTC   = "TBTC    " --Testnet Bitcoin
-codeERGO   :: ByteString
-codeERGO   = "ERGO    " --Ergo
-codeTERGO  :: ByteString
-codeTERGO  = "TERGO   " --Testnet Ergo
-codeUSDTO  :: ByteString
-codeUSDTO  = "USDTO   " --Omni USDT
-codeTUSDTO :: ByteString
-codeTUSDTO = "TUSDTO  " --Testnet Omni USDT
-codeLT     :: ByteString
-codeLT     = "LT      " --Litecoin
-codeTLT    :: ByteString
-codeTLT    = "TLT     " --Testnet Litecoin
-codeZEC    :: ByteString
-codeZEC    = "ZEC     " --ZCash
-codeTZEC   :: ByteString
-codeTZEC   = "TZEC    " --Testnet ZCash
-codeCPR    :: ByteString
-codeCPR    = "CPR     " --Cypra token
-codeTCPR   :: ByteString
-codeTCPR   = "TCPR    " --Testnet Cypra
+currencyTag :: CurrencyCode -> Word32
+currencyTag = \case
+  BTC    -> 0
+  TBTC   -> 1
+  ERGO   -> 2
+  TERGO  -> 3
+  USDTO  -> 4
+  TUSDTO -> 5
+  LTC    -> 6
+  TLTC   -> 7
+  ZEC    -> 8
+  TZEC   -> 9
+  CPR    -> 10
+  TCPR   -> 11
+  DASH   -> 12
+  TDASH  -> 13
 
-word32sizeOf :: (Storable a) => a -> Word32
-word32sizeOf = fromIntegral . sizeOf
+genericSizeOf :: (Storable a, Integral b) => a -> b
+genericSizeOf = fromIntegral . sizeOf
 
-scanBlock :: Word32 -> Word32 -> Word64 -> Word64 -> Builder
-scanBlock currency version scanHeight height = word32BE currency
+scanBlock :: CurrencyCode -> Word32 -> Word64 -> Word64 -> Builder
+scanBlock currency version scanHeight height = word32BE (currencyTag currency)
                                             <> word32BE version
                                             <> word64BE scanHeight
                                             <> word64BE height
@@ -68,7 +74,7 @@ verMsg version time nonce scanBlocks =
     msgType = 0
     scanBlockSize = 24
     currenciesAmount = fromIntegral $ V.length scanBlocks
-    msgSize = word32sizeOf time + word32sizeOf nonce + currenciesAmount * scanBlockSize
+    msgSize = genericSizeOf time + genericSizeOf nonce + currenciesAmount * scanBlockSize
 
 verACKMsg :: Builder
 verACKMsg = msg msgType msgSize mempty
@@ -79,14 +85,14 @@ verACKMsg = msg msgType msgSize mempty
 pingMsg :: Word64 -> Builder
 pingMsg nonce = msg msgType msgSize $ word64BE nonce
   where
-    msgType = word32sizeOf nonce
+    msgType = genericSizeOf nonce
     msgSize = 4
 
 pongMsg :: Word64 -> Builder
 pongMsg nonce = msg msgType msgSize $ word64BE nonce
   where
     msgType = 12
-    msgSize = word32sizeOf nonce
+    msgSize = genericSizeOf nonce
 
 msg :: Word32 -> Word32 -> Builder -> Builder
 msg msgType msgLength payload = word32BE msgType <> word32BE msgLength <> payload 
