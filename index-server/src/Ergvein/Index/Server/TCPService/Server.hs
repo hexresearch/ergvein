48dbcdee2664479d3fb6729e9a3a6b7d68e63ba8
Aminion <aminion@protonmail.com>
2020-07-19 11:24:46 +0300

    Lift to ServerM #511

--- index-server/src/Ergvein/Index/Server/TCPService/Server.hs
+++ index-server/src/Ergvein/Index/Server/TCPService/Server.hs
@@ -6,36 +6,45 @@ import Data.ByteString.Builder
 import Network.Socket
 import System.IO
 import Data.Word
-
+import Data.Maybe
 import Ergvein.Index.Protocol.Deserialization
 import Ergvein.Index.Protocol.Serialization
 import Ergvein.Index.Protocol.Types
+import Ergvein.Index.Server.Monad
+import Control.Monad.IO.Unlift
 
 import qualified Network.Socket.ByteString as NS
 
-tcpSrv = withSocketsDo $ do
-  sock <- socket AF_INET Stream 0
-  setSocketOption sock ReuseAddr 1
-  bind sock (SockAddrInet 4242 iNADDR_ANY)
-  listen sock 2
-  mainLoop sock
+tcpSrv :: ServerM ()
+tcpSrv = do
+  unlift <- askUnliftIO
+  liftIO $ withSocketsDo $ do
+    sock <- socket AF_INET Stream 0
+    setSocketOption sock ReuseAddr 1
+    bind sock (SockAddrInet 4242 iNADDR_ANY)
+    listen sock 2
+    unliftIO unlift $ mainLoop sock
 
-mainLoop :: Socket -> IO ()
+mainLoop :: Socket -> ServerM ()
 mainLoop sock = do
-  conn <- accept sock
-  forkIO $ runConn conn
+  conn <- liftIO $ accept sock
+  forkIO <$> (toIO $ runConn conn)
   mainLoop sock
 
-runConn :: (Socket, SockAddr) -> IO ()
+runConn :: (Socket, SockAddr) -> ServerM ()
 runConn (sock, _) = do
-  h <- maybeResult . (parse messageHeaderParser) <$> NS.recv sock 8
+  h <- liftIO $ maybeResult . (parse messageHeaderParser) <$> NS.recv sock 8
   case h of
     Just MessageHeader {..} -> do
-      msg <- maybeResult . (parse $ messageParser msgType) <$> (NS.recv sock $ fromIntegral msgSize)
-      hdl <- socketToHandle sock ReadWriteMode
+      msg <- liftIO $ fromJust <$> maybeResult . (parse $ messageParser msgType) <$> (NS.recv sock $ fromIntegral msgSize)
+      resp <- handleMsg msg
+      liftIO $ do
+        hdl <- socketToHandle sock ReadWriteMode
       --hPutBuilder hdl msg
-      hClose hdl
-      pure ()
+        hClose hdl
     Nothing -> do
-      close sock
+      liftIO $ close sock
       pure ()
+
+handleMsg :: Message -> ServerM Message
+handleMsg = undefined
