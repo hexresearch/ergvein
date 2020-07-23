db1323afe1d8cd8d6eec8a93e4e86710c91cb8cd
lemarwin <lemarwin42@gmail.com>
2020-07-23 20:29:12 +0200

    close #535

--- wallet/src/Ergvein/Wallet/Page/Initial.hs
+++ wallet/src/Ergvein/Wallet/Page/Initial.hs
@@ -4,29 +4,18 @@ module Ergvein.Wallet.Page.Initial(
     initialPage
   ) where
 
-import Control.Lens
-import Control.Monad.IO.Class
-import Ergvein.Types.AuthInfo
-import Ergvein.Types.Currency
-import Ergvein.Types.Keys
 import Ergvein.Types.Storage
 import Ergvein.Wallet.Alert
 import Ergvein.Wallet.Elements
 import Ergvein.Wallet.Language
 import Ergvein.Wallet.Localization.Initial
-import Ergvein.Wallet.Localization.Storage
 import Ergvein.Wallet.Monad
 import Ergvein.Wallet.Native
 import Ergvein.Wallet.Page.Password
 import Ergvein.Wallet.Page.Seed
 import Ergvein.Wallet.Storage.AuthInfo
-import Ergvein.Wallet.Storage.Keys
 import Ergvein.Wallet.Storage.Util
 import Ergvein.Wallet.Wrapper
-
-import qualified Data.Map.Strict       as M
-import qualified Data.Map.Merge.Strict as MM
-import qualified Data.Vector           as V
 
 data GoPage = GoSeed | GoRestore
 
@@ -87,56 +76,3 @@ loadWalletPage name = do
   mAuthE <- performEvent $ generateMissingPrvKeys <$> oldAuthE
   authE <- handleDangerMsg mAuthE
   void $ setAuthInfo $ Just <$> authE
-
--- | Generates new private keys until their number is equal to the number of public keys.
-generateMissingPrvKeys :: MonadIO m => (AuthInfo, Password) -> m (Either StorageAlert AuthInfo)
-generateMissingPrvKeys (authInfo, pass) = do
-  let encryptedPrvStorage = view (authInfo'storage . storage'encryptedPrvStorage) authInfo
-  case decryptPrvStorage encryptedPrvStorage pass of
-    Left err -> pure $ Left err
-    Right decryptedPrvStorage -> do
-      let updatedPrvStorage = set prvStorage'currencyPrvStorages updatedPrvKeystore decryptedPrvStorage
-      encryptPrvStorageResult <- encryptPrvStorage updatedPrvStorage pass
-      case encryptPrvStorageResult of
-        Left err -> pure $ Left err
-        Right encryptedUpdatedPrvStorage -> pure $ Right $ set (authInfo'storage . storage'encryptedPrvStorage) encryptedUpdatedPrvStorage authInfo
-      where
-        currencyPrvStorages = view prvStorage'currencyPrvStorages decryptedPrvStorage
-        currencyPubStorages = view (authInfo'storage . storage'pubStorage . pubStorage'currencyPubStorages) authInfo
-        pubKeysNumber = M.map (\currencyPubStorage -> (
-            V.length $ pubKeystore'external (view currencyPubStorage'pubKeystore currencyPubStorage),
-            V.length $ pubKeystore'internal (view currencyPubStorage'pubKeystore currencyPubStorage)
-          )) currencyPubStorages
-        updatedPrvKeystore =
-          MM.merge
-          MM.dropMissing
-          MM.dropMissing
-          (MM.zipWithMatched generateMissingPrvKeysHelper)
-          currencyPrvStorages
-          pubKeysNumber
-
-generateMissingPrvKeysHelper ::
-  Currency
-  -> CurrencyPrvStorage -- ^ Private keystore
-  -> (Int, Int)         -- ^ Total number of external and internal private keys respectively that should be stored in keystore
-  -> CurrencyPrvStorage -- ^ Updated private keystore
-generateMissingPrvKeysHelper _ (CurrencyPrvStorage prvKeystore) (goalExternalKeysNum, goalInternalKeysNum) =
-  CurrencyPrvStorage $ PrvKeystore masterPrvKey updatedExternalPrvKeys updatedInternalPrvKeys
-  where
-    currentExternalKeys = prvKeystore'external prvKeystore
-    currentInternalKeys = prvKeystore'internal prvKeystore
-    masterPrvKey = prvKeystore'master prvKeystore
-    extLength = V.length currentExternalKeys
-    intLength = V.length currentInternalKeys
-    updatedExternalPrvKeys = if extLength >= goalExternalKeysNum
-      then currentExternalKeys
-      else let
-        l = goalExternalKeysNum - extLength
-        v = V.unfoldrN l (\i -> Just (derivePrvKey masterPrvKey External (fromIntegral i), i+1)) extLength
-        in currentExternalKeys V.++ v
-    updatedInternalPrvKeys = if intLength >= goalInternalKeysNum
-      then currentInternalKeys
-      else let
-        l = goalInternalKeysNum - intLength
-        v = V.unfoldrN l (\i -> Just (derivePrvKey masterPrvKey Internal (fromIntegral i), i+1)) intLength
-        in currentInternalKeys V.++ v
