bedb3c942473f61bea6da50753585fb1862b5eed
Levon Oganyan <lemarwin42@gmail.com>
2020-07-24 09:14:18 +0200

    close #567 (#568)
    * close #567
    
    * close #539

--- wallet/src/Ergvein/Wallet/Menu/Types.hs
+++ wallet/src/Ergvein/Wallet/Menu/Types.hs
@@ -2,11 +2,14 @@ module Ergvein.Wallet.Menu.Types(
     MenuItem (..)
   ) where
 
+import Ergvein.Text
+import Ergvein.Types.Currency
 import Ergvein.Wallet.Language
 import Ergvein.Wallet.Monad
 
 data MenuItem
   = MenuBalances
+  | MenuSingleBalance Currency
   | MenuNetwork
   | MenuSettings
   | MenuAbout
@@ -17,6 +20,7 @@ instance LocalizedPrint MenuItem where
   localizedShow l v = case l of
     English -> case v of
       MenuBalances    -> "Balances"
+      MenuSingleBalance cur -> showt cur <>  " balance"
       MenuNetwork     -> "Network"
       MenuSettings    -> "Settings"
       MenuAbout       -> "About"
@@ -24,6 +28,7 @@ instance LocalizedPrint MenuItem where
       MenuSwitch      -> "Switch wallet"
     Russian -> case v of
       MenuBalances    -> "Балансы"
+      MenuSingleBalance cur -> "Баланс" <> showt cur
       MenuNetwork     -> "Сеть"
       MenuSettings    -> "Настройки"
       MenuAbout       -> "О программе"
