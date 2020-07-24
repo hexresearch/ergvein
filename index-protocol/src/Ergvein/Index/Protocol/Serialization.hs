63d733d5b4e71234f86082e52900497a848df51c
Aminion <aminion@protonmail.com>
2020-07-24 17:59:15 +0300

    Version message serialization #494

--- index-protocol/src/Ergvein/Index/Protocol/Serialization.hs
+++ index-protocol/src/Ergvein/Index/Protocol/Serialization.hs
@@ -1,9 +1,11 @@
 module Ergvein.Index.Protocol.Serialization where
 
 import Data.ByteString.Builder
+import Data.Monoid
 import Data.Word
 import Ergvein.Index.Protocol.Types
-import qualified Data.Vector as V
+
+import qualified Data.Vector.Unboxed as V
 
 messageTypeToWord32 :: MessageType -> Word32 
 messageTypeToWord32 = \case
@@ -62,16 +64,58 @@ pongMsg nonce = msg Pong msgSize $ word64BE nonce
 messageBase :: MessageType -> Word32 -> Builder -> Builder
 messageBase msgType msgLength payload = word32BE (messageTypeToWord32 msgType) <> word32BE msgLength <> payload
 
+
+
+scanBlockBuilder :: ScanBlock -> (Sum Word32, Builder)
+scanBlockBuilder (ScanBlock {..}) = (scanBlockSize, scanBlock)
+  where
+    scanBlockSize = Sum $
+        genericSizeOf (currencyCodeToWord32 scanBlockCurrency) 
+      + genericSizeOf scanBlockVersion
+      + genericSizeOf scanBlockScanHeight
+      + genericSizeOf scanBlockHeight
+
+    scanBlock = 
+        (word32BE $ currencyCodeToWord32 scanBlockCurrency)
+      <> word32BE scanBlockVersion
+      <> word64BE scanBlockScanHeight
+      <> word64BE scanBlockHeight
+
+
+
 messageBuilder :: Message -> Builder
 
 messageBuilder (PingMsg msg) = messageBase Ping msgSize $ word64BE msg
   where
     msgSize = genericSizeOf msg
 
-messageBuilder (PongMsg   msg) = messageBase Pong msgSize $ word64BE msg
+messageBuilder (PongMsg msg) = messageBase Pong msgSize $ word64BE msg
   where
     msgSize = genericSizeOf msg
 
 messageBuilder (RejectMsg msg) = messageBase Reject msgSize $ word32BE $ rejectTypeToWord32 $ rejectMsgCode msg
   where
     msgSize = genericSizeOf $ rejectTypeToWord32 $ rejectMsgCode msg
+
+messageBuilder (VersionACKMsg msg) = messageBase VersionACK msgSize $ mempty
+  where
+    msgSize = 0
+
+messageBuilder (VersionMsg (VersionMessage {..})) = let 
+  (scanBlocksSizeSum, scanBlocks) = mconcat $ (scanBlockBuilder <$> V.toList versionMsgScanBlocks)
+  scanBlocksSize = getSum scanBlocksSizeSum
+  time = round versionMsgTime
+  msgSize = genericSizeOf versionMsgVersion
+          + genericSizeOf time
+          + genericSizeOf versionMsgNonce
+          + genericSizeOf versionMsgCurrencies
+          + genericSizeOf scanBlocksSize
+          + scanBlocksSize
+
+  in messageBase Version msgSize 
+    $  word32BE versionMsgVersion
+    <> word64BE time
+    <> word64BE versionMsgNonce
+    <> word32BE versionMsgCurrencies
+    <> word32BE scanBlocksSize
+    <> scanBlocks
