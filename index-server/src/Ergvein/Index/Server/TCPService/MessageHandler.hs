3f87486bba198156321eaa52e1631bbbe2a9a825
lemarwin <lemarwin42@gmail.com>
2020-08-11 17:42:35 +0200

    handle fee messages

--- index-server/src/Ergvein/Index/Server/TCPService/MessageHandler.hs
+++ index-server/src/Ergvein/Index/Server/TCPService/MessageHandler.hs
@@ -1,16 +1,23 @@
 module Ergvein.Index.Server.TCPService.MessageHandler where
 
-import Ergvein.Index.Protocol.Types
-import Ergvein.Index.Server.Monad
+import Control.Concurrent.STM
+import Control.Monad.IO.Class
+import Control.Monad.Reader
+import Conversion
+
+import Ergvein.Index.Server.Environment
+import Ergvein.Index.API.Types
+import Ergvein.Index.Protocol.Types as IPT
 import Ergvein.Index.Server.DB.Queries
 import Ergvein.Index.Server.DB.Schema
-import Ergvein.Index.API.Types
+import Ergvein.Index.Server.Monad
+import Ergvein.Index.Server.TCPService.Conversions
 import Ergvein.Types.Currency
 import Ergvein.Types.Fees
 import Ergvein.Types.Transaction
+
+import qualified Data.Map.Strict as M
 import qualified Data.Vector as V
-import Conversion
-import Ergvein.Index.Server.TCPService.Conversions
 
 getBlockMetaSlice :: Currency -> BlockHeight -> BlockHeight -> ServerM [BlockMetaRec]
 getBlockMetaSlice currency startHeight endHeight = do
@@ -29,7 +36,14 @@ handleMsg (FiltersRequestMsg FilterRequestMessage {..}) = do
   slice <- getBlockMetaSlice undefined filterRequestMsgStart filterRequestMsgAmount
   let filters = V.fromList $ convert <$> slice
 
-  pure $ Just $ FiltersResponseMsg $ FilterResponseMessage 
+  pure $ Just $ FiltersResponseMsg $ FilterResponseMessage
     { filterResponseCurrency = filterRequestMsgCurrency
     , filterResponseFilters = filters
     }
+
+handleMsg (FeeRequestMsg FeeRequestMessage{..}) = do
+  fees <- liftIO . readTVarIO =<< asks envFeeEstimates
+  let mfee = fmap (extractFee feeRequestLevel) $ M.lookup feeRequestCurrency fees
+  pure $ flip fmap mfee $ \(c,e) -> FeeResponseMsg $ case feeRequestCurrency of
+    IPT.BTC -> FeeResponseBTC c e
+    _ -> FeeResponseGeneric feeRequestCurrency e
