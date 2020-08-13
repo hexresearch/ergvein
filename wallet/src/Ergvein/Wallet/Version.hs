6a238b15ac1e26584b3a9249c3e55ae7f3f21612
Anton Gushcha <ncrashed@gmail.com>
2020-08-13 12:48:38 +0300

    WIP version

--- wallet/src/Ergvein/Wallet/Version.hs
+++ wallet/src/Ergvein/Wallet/Version.hs
@@ -6,8 +6,9 @@ import Foreign.C.String
 import Foreign.C.Types
 import Foreign.Ptr
 
+import Debug.Trace
+
 foreign import ccall "get_binary_version_start" binaryVersionStart :: Ptr CChar
-foreign import ccall "get_binary_version_size" binaryVersionSize :: Int
 
 getVersionRaw :: IO String
-getVersionRaw = peekCStringLen (binaryVersionStart, binaryVersionSize)
+getVersionRaw = peekCString binaryVersionStart
