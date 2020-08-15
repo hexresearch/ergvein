043b782382901ac8f78685c10fedfa4344e10ea7
lemarwin <lemarwin42@gmail.com>
2020-08-15 18:03:57 +0200

    Settings.Network page done

--- wallet/src/Ergvein/Wallet/Page/Settings/Network.hs
+++ wallet/src/Ergvein/Wallet/Page/Settings/Network.hs
@@ -6,11 +6,14 @@ module Ergvein.Wallet.Page.Settings.Network
   ) where
 
 import Control.Lens
+import Control.Monad.IO.Class
 import Data.Bifunctor
-import Data.Maybe (isJust, fromJust)
+import Data.Functor.Misc (Const2(..))
+import Data.Maybe (isJust, fromJust, listToMaybe)
 import Data.Time
-import Network.Socket (SockAddr(..))
+import Network.Socket
 import Reflex.Dom
+import Reflex.ExternalRef
 import Servant.Client(BaseUrl, showBaseUrl, parseBaseUrl)
 import Text.Read
 
@@ -18,6 +21,7 @@ import Ergvein.Text
 import Ergvein.Wallet.Alert
 import Ergvein.Wallet.Clipboard
 import Ergvein.Wallet.Elements
+import Ergvein.Wallet.Indexer.Socket
 import Ergvein.Wallet.Input
 import Ergvein.Wallet.Language
 import Ergvein.Wallet.Localization.Settings
@@ -26,6 +30,7 @@ import Ergvein.Wallet.Monad
 import Ergvein.Wallet.Settings
 import Ergvein.Wallet.Wrapper
 
+import qualified Control.Exception.Safe as Ex
 import qualified Data.Map.Strict as M
 import qualified Data.Text as T
 import qualified Data.Set as S
@@ -103,103 +108,122 @@ parametersPageWidget = mdo
   where
     fmap2 = fmap . fmap
 
-addUrlWidget :: forall t m . MonadFront t m => Dynamic t Bool -> m (Event t BaseUrl)
-addUrlWidget showD = mdo
-  hideE <- fmap switchDyn $ widgetHoldDyn $ ffor showD $ \b -> if not b then pure never else do
-    murlE <- lineOption $ do
-      textD <- fmap _inputElement_value $ inputElement $ def
-        & inputElementConfig_elementConfig . elementConfig_initialAttributes .~ ("type" =: "text")
-      goE <- outlineButton NSSAddUrl
-      let urlE :: Event t (Maybe BaseUrl) = fmap (parseBaseUrl . T.unpack) $ current textD `tag` goE
-      pure urlE
-    widgetHold (pure ()) $ ffor murlE $ \case
-      Nothing -> divClass "form-field-errors" $ text "Falied to parse URL"
-      _ -> pure ()
-    pure $ fmapMaybe id murlE
-  pure hideE
+addUrlWidget :: forall t m . MonadFront t m => Dynamic t Bool -> m (Event t SockAddr)
+addUrlWidget showD = fmap switchDyn $ widgetHoldDyn $ ffor showD $ \b -> if not b then pure never else do
+  murlE <- el "div" $ do
+    textD <- fmap _inputElement_value $ inputElement $ def
+      & inputElementConfig_elementConfig . elementConfig_initialAttributes .~ ("type" =: "text")
+    goE <- outlineButton NSSAddUrl
+    performFork $ ffor goE $ const $ do
+      t <- sampleDyn textD
+      let (h,p) = fmap (T.drop 1) $ T.span (/= ':') t
+      let p' = if p == "" then Nothing else Just $ T.unpack p
+      let hints = defaultHints { addrFlags = [AI_ALL] , addrSocketType = Stream }
+      addrs <- liftIO $ Ex.catch (
+          getAddrInfo (Just hints) (Just $ T.unpack h) p'
+        ) (\(_ :: Ex.SomeException) -> pure [])
+      pure $ fmap addrAddress $ listToMaybe addrs
+  widgetHold (pure ()) $ ffor murlE $ \case
+    Nothing -> divClass "form-field-errors" $ text "Falied to parse URL"
+    _ -> pure ()
+  pure $ fmapMaybe id murlE
 
 activePageWidget :: forall t m . MonadFront t m => m ()
-activePageWidget = pure ()
-{-
-activePageWidget :: forall t m . MonadFront t m => m ()
 activePageWidget = mdo
+  connsD  <- externalRefDynamic =<< getActiveConnsRef
+  addrsD <- (fmap . fmap) settingsActiveSockAddrs getSettingsD
   showD <- holdDyn False $ leftmost [False <$ hideE, tglE]
-  void . flip listWithKey renderActive =<< getIndexerInfoD
+  let valsD = (,) <$> connsD <*> addrsD
+  widgetHoldDyn $ ffor valsD $ \(conmap, urls) ->
+    flip traverse urls $ \sa -> renderActive sa refrE $ M.lookup sa conmap
   hideE <- activateURL =<< addUrlWidget showD
-  tglE <- divClass "network-wrapper mt-3" $ divClass "net-btns-3" $ do
-    refreshIndexerInfo =<< buttonClass "button button-outline m-0" NSSRefresh
-    restoreDefaultIndexers =<< buttonClass "button button-outline m-0" NSSRestoreUrls
-    fmap switchDyn $ widgetHoldDyn $ ffor showD $ \b ->
+  (refrE, tglE, restoreE) <- divClass "network-wrapper mt-3" $ divClass "net-btns-3" $ do
+    refrE <- buttonClass "button button-outline m-0" NSSRefresh
+    restoreE <- buttonClass "button button-outline m-0" NSSRestoreUrls
+    tglE <- fmap switchDyn $ widgetHoldDyn $ ffor showD $ \b ->
       fmap (not b <$) $ buttonClass "button button-outline m-0" $ if b then NSSClose else NSSAddUrl
+    pure (refrE, tglE, restoreE)
   pure ()
--}
-renderActive :: MonadFront t m => SockAddr -> Dynamic t (Maybe IndexerInfo) -> m ()
-renderActive url minfoD = mdo
+
+renderActive :: MonadFront t m
+  => SockAddr
+  -> Event t ()
+  -> (Maybe (IndexerConnection t))
+  -> m ()
+renderActive addr refrE mconn = mdo
   tglD <- holdDyn False tglE
-  tglE <- fmap switchDyn $ widgetHoldDyn $ ffor minfoD $ \minfo -> lineOption $ do
+  tglE <- lineOption $ do
     tglE' <- divClass "network-name" $ do
-      let cls = if isJust minfo then "mt-a mb-a indexer-online" else "mt-a mb-a indexer-offline"
+      let cls = if isJust mconn then "mt-a mb-a indexer-online" else "mt-a mb-a indexer-offline"
       elClass "span" cls $ elClass "i" "fas fa-circle" $ pure ()
-      divClass "mt-a mb-a network-name-txt" $ text $ showt url
+      divClass "mt-a mb-a network-name-txt" $ text $ showt addr
       fmap switchDyn $ widgetHoldDyn $ ffor tglD $ \b ->
         fmap (not b <$) $ buttonClass "button button-outline network-edit-btn mt-a mb-a ml-a" $ if b then NSSClose else NSSEdit
-    descrOption $ maybe NSSOffline (NSSLatency . indInfoLatency) minfo
+    case mconn of
+      Nothing -> descrOption NSSOffline
+      Just conn -> do
+        latD <- indexerConnPingerWidget conn refrE
+        descrOptionDyn $ NSSLatency <$> latD
     pure tglE'
   widgetHoldDyn $ ffor tglD $ \b -> if not b
     then pure ()
     else divClass "network-wrapper mt-2" $ do
-      deactivateURL . (url <$) =<< buttonClass "button button-outline mt-1 ml-1" NSSDisable
-      forgetURL . (url <$) =<< buttonClass "button button-outline mt-1 ml-1" NSSForget
+      deactivateURL . (addr <$) =<< buttonClass "button button-outline mt-1 ml-1" NSSDisable
+      forgetURL . (addr <$) =<< buttonClass "button button-outline mt-1 ml-1" NSSForget
       pure ()
   pure ()
 
 inactivePageWidget :: forall t m . MonadFront t m => m ()
 inactivePageWidget = mdo
-  -- urlsD <- (fmap . fmap) S.toList getInactiveUrlsD
-  -- showD <- holdDyn False $ leftmost [False <$ hideE, tglE]
-  -- allResE <- fmap switchDyn $ widgetHoldDyn $ ffor urlsD $ \urls ->
-  --   fmap (mergeMap . M.fromList) $ flip traverse urls $ \u -> do
-  --     resE <- pingIndexer $ u <$ pingAllE
-  --     pure (u, snd <$> resE)
-  --
-  -- infomapD <- foldDyn M.union M.empty $ leftmost [resE, allResE]
-  -- a :: Dynamic t [Dynamic t (Event t BaseUrl)] <- simpleList urlsD $ \urlD -> do
-  --   let myInfoD = M.lookup <$> urlD <*> infomapD
-  --   widgetHoldDyn $ renderInactive <$> urlD <*> myInfoD
-  -- let pingE = switchDyn . fmap leftmost . join . fmap sequence $ a
-  -- resE <- (fmap . fmap) (uncurry M.singleton) $ pingIndexer pingE
-  -- hideE <- deactivateURL =<< addUrlWidget showD
-  -- (pingAllE, tglE) <- divClass "network-wrapper mt-1" $ divClass "net-btns-2" $ do
-  --   pingAllE <- buttonClass "button button-outline m-0" NSSPingAll
-  --   tglE <- fmap switchDyn $ widgetHoldDyn $ ffor showD $ \b ->
-  --     fmap (not b <$) $ buttonClass "m-0 button button-outline" $ if b then NSSClose else NSSAddUrl
-  --   pure (pingAllE, tglE)
+  addrsD <- externalRefDynamic =<< getInactiveUrlsRef
+  showD <- holdDyn False $ leftmost [False <$ hideE, tglE]
+  hideE <- deactivateURL =<< addUrlWidget showD
+  let addrsMapD = (M.fromList . fmap (,()) . S.toList) <$> addrsD
+  listWithKey addrsMapD $ \addr _ -> renderInactive pingAllE addr
+  (pingAllE, tglE) <- divClass "network-wrapper mt-1" $ divClass "net-btns-2" $ do
+    pingAllE <- buttonClass "button button-outline m-0" NSSPingAll
+    tglE <- fmap switchDyn $ widgetHoldDyn $ ffor showD $ \b ->
+      fmap (not b <$) $ buttonClass "m-0 button button-outline" $ if b then NSSClose else NSSAddUrl
+    pure (pingAllE, tglE)
   pure ()
 
-renderInactive :: MonadFront t m => SockAddr -> Maybe (Maybe IndexerInfo) -> m (Event t SockAddr)
-renderInactive url mminfo = mdo
-  let urlTxt = showt url
+renderInactive :: MonadFront t m => Event t () -> SockAddr -> m ()
+renderInactive initPingE addr = mdo
+  sel <- getIndexReqSelector
   tglD <- holdDyn False tglE
-  tglE <- lineOption $ do
-    tglE' <- divClass "network-name" $ do
-      case mminfo of
-        Nothing -> divClass "mt-a mb-a network-name-txt" $ text urlTxt
-        Just minfo -> do
-          let cls = if isJust minfo then "mt-a mb-a indexer-online" else "mt-a mb-a indexer-offline"
+  (fstPingE, refrE) <- headTailE $ leftmost [initPingE, pingE]
+  tglE <- fmap switchDyn $ lineOption $ widgetHold (startingWidget tglD) $ ffor fstPingE $ const $ do
+      let reqE = select sel $ Const2 addr
+      conn <- initIndexerConnection addr reqE
+      pingD <- indexerConnPingerWidget conn refrE
+      fmap switchDyn $ widgetHoldDyn $ ffor pingD $ \p -> do
+        tglE' <- divClass "network-name" $ do
+          let cls = if p == 0 then "mt-a mb-a indexer-offline" else "mt-a mb-a indexer-online"
           elClass "span" cls $ elClass "i" "fas fa-circle" $ pure ()
-          divClass "mt-a mb-a network-name-txt" $ text urlTxt
-      fmap switchDyn $ widgetHoldDyn $ ffor tglD $ \b ->
-        fmap (not b <$) $ buttonClass "button button-outline network-edit-btn mt-a mb-a ml-a" $ if b then NSSClose else NSSEdit
-    maybe (pure ()) (descrOption . maybe NSSOffline (NSSLatency . indInfoLatency)) mminfo
-    pure tglE'
+          divClass "mt-a mb-a network-name-txt" $ text $ showt addr
+          tglBtn tglD
+        descrOption $ NSSLatency p
+        pure tglE'
   pingE <- fmap switchDyn $ widgetHoldDyn $ ffor tglD $ \b -> if not b
     then pure never
     else divClass "network-wrapper mt-1" $ divClass "net-btns-3" $ do
-      activateURL . (url <$) =<< outlineButton NSSEnable
-      pingE <- fmap (url <$) $ outlineButton NSSPing
-      forgetURL . (url <$) =<< outlineButton NSSForget
+      activateURL . (addr <$) =<< outlineButton NSSEnable
+      pingE <- outlineButton NSSPing
+      forgetURL . (addr <$) =<< outlineButton NSSForget
       pure pingE
-  pure pingE
+  pure ()
+  where
+    tglBtn :: MonadFront t m => Dynamic t Bool -> m (Event t Bool)
+    tglBtn tglD = fmap switchDyn $ widgetHoldDyn $ ffor tglD $ \b ->
+      fmap (not b <$) $ buttonClass "button button-outline network-edit-btn mt-a mb-a ml-a" $
+        if b then NSSClose else NSSEdit
+
+    startingWidget tglD = do
+      tglE <- divClass "network-name" $ do
+        divClass "mt-a mb-a network-name-txt" $ text $ showt addr
+        tglBtn tglD
+      descrOption NSSOffline
+      pure tglE
 
 navbarWidget :: MonadFront t m => NavbarItem -> m (Dynamic t NavbarItem)
 navbarWidget initItem = divClass "navbar" $ mdo
