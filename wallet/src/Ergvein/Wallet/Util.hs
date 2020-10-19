a7a8a8901e0475a17183ceabff651c6d672d8639
fastpok <krutkinvs@gmail.com>
2020-10-19 17:43:21 +0300

    Fix currencyToCurrencyCode

--- wallet/src/Ergvein/Wallet/Util.hs
+++ wallet/src/Ergvein/Wallet/Util.hs
@@ -20,8 +20,10 @@ module Ergvein.Wallet.Util(
 import Control.Monad.Except
 import Reflex.Dom
 
-import qualified Ergvein.Types.Currency as ETC
 import Ergvein.Index.Protocol.Types
+import Ergvein.Wallet.Platform
+
+import qualified Ergvein.Types.Currency as ETC
 
 -- | Same as 'widgetHold' but for dynamic
 widgetHoldDyn :: forall t m a . (Reflex t, Adjustable t m, MonadHold t m) => Dynamic t (m a) -> m (Dynamic t a)
@@ -86,8 +88,8 @@ eventToNextFrameN' n evtM = do
 
 currencyToCurrencyCode :: ETC.Currency -> CurrencyCode
 currencyToCurrencyCode c = case c of
-  ETC.BTC -> BTC
-  ETC.ERGO -> ERGO
+  ETC.BTC -> if isTestnet then TBTC else BTC
+  ETC.ERGO -> if isTestnet then TERGO else ERGO
 
 currencyCodeToCurrency :: CurrencyCode -> ETC.Currency
 currencyCodeToCurrency c = case c of
