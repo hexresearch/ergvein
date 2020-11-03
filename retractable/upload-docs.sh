c676b8336f58c8e8656dbf693bb500cd46521da4
Anton Gushcha <ncrashed@protonmail.com>
2020-11-04 02:59:41 +0300

    Update script for uploading docs to hackage

--- retractable/upload-docs.sh
+++ retractable/upload-docs.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
-set -e
-
-dir=$(mktemp -d dist-docs.XXXXXX)
-trap 'rm -r "$dir"' EXIT
+set -xe
 
 # assumes cabal 2.4 or later
-cabal v2-haddock --builddir="$dir" --haddock-for-hackage --enable-doc
+cabal v2-haddock --haddock-for-hackage --enable-doc
 
-cabal upload -d --publish $dir/*-docs.tar.gz
+cabal upload -d --publish ../dist-newstyle/reflex-dom-retractable-*.tar.gz
