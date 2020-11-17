76e29dfcff323910f3ac08cf08a94d9c5f6d3d9b
lemarwin <lemarwin42@gmail.com>
2020-11-18 01:00:42 +0300

    Display restore status

--- wallet/src/Ergvein/Wallet/Page/Restore.hs
+++ wallet/src/Ergvein/Wallet/Page/Restore.hs
@@ -1,32 +1,28 @@
+{-# OPTIONS_GHC -Wall #-}
 module Ergvein.Wallet.Page.Restore(
     restorePage
   ) where
 
 import Control.Concurrent.Async
 import Control.Monad.IO.Class
-import Data.ByteString (ByteString)
-import Data.Foldable (foldl')
-import Data.Maybe (fromMaybe, catMaybes)
-import Reflex.ExternalRef
+import Data.Maybe (catMaybes)
+import Reflex.Localize
 
 import Ergvein.Filters.Btc.Index
-import Ergvein.Filters.Mutable hiding (BlockHeight, BlockHash)
-import Ergvein.Text
+import Ergvein.Filters.Mutable hiding (BlockHeight)
 import Ergvein.Types.Currency
-import Ergvein.Types.Derive
 import Ergvein.Types.Keys
 import Ergvein.Types.Storage
 import Ergvein.Types.Transaction
 import Ergvein.Wallet.Elements
 import Ergvein.Wallet.Filters.Loader
+import Ergvein.Wallet.Localization.Restore
 import Ergvein.Wallet.Monad
 import Ergvein.Wallet.Native
 import Ergvein.Wallet.Node.Types
 import Ergvein.Wallet.Page.Balances
 import Ergvein.Wallet.Platform
 import Ergvein.Wallet.Scan
-import Ergvein.Wallet.Storage.Constants
-import Ergvein.Wallet.Storage.Util
 import Ergvein.Wallet.Sync.Status
 import Ergvein.Wallet.Sync.Widget
 import Ergvein.Wallet.Wrapper
@@ -34,16 +30,22 @@ import Ergvein.Wallet.Wrapper
 import qualified Data.Map.Strict as M
 import qualified Data.Text as T
 import qualified Data.Vector as V
-import qualified Network.Haskoin.Block              as HB
-import qualified Network.Haskoin.Transaction        as HT
 
 restorePage :: forall t m . MonadFront t m =>  m ()
 restorePage = wrapperSimple True $ void $ workflow nodeConnection
   where
+    restoreProgressWidget :: BlockHeight -> BlockHeight -> BlockHeight -> m ()
+    restoreProgressWidget f0' fh' hh' = h2 $ localizedText $ RPSProgress p
+      where
+        [f0, fh, hh] = fromIntegral <$> [f0',fh',hh']
+        p = 100 * (fh - f0) / (hh - f0)
+
+    filtersBatchSize :: Int
     filtersBatchSize = 700
+
     -- | Stage 1: connect to BTC nodes
     nodeConnection = Workflow $ do
-      el "h3" $ text "Connecting to nodes"
+      h2 $ localizedText RPSConnecting
       syncWidget False BTC
       conmapD <- getNodesByCurrencyD BTC
       let upsD = fmap or $ join $ ffor conmapD $ \cm -> sequence $ ffor (M.elems cm) $ \case
@@ -54,7 +56,7 @@ restorePage = wrapperSimple True $ void $ workflow nodeConnection
 
     -- | Stage 2: calculate the current height
     heightAsking = Workflow $ do
-      el "h3" $ text "Getting current height"
+      h2 $ localizedText RPSGetHeight
       syncWidget False BTC
       heightD <- getCurrentHeight BTC
       height0E <- tag (current heightD) <$> getPostBuild
@@ -65,10 +67,14 @@ restorePage = wrapperSimple True $ void $ workflow nodeConnection
     -- | Stage 3: get a batch of filters and send them to stage 4
     -- if filters height >= btc height - 1, goto stage 5
     getFiltersBatch = Workflow $ do
-      fh <- getScannedHeight BTC
-      h3 $ text $ "Getting a fresh batch of filters from: " <> showt fh
-      psD <- getPubStorageD
       heightD <- getCurrentHeight BTC
+      fh <- getScannedHeight BTC
+      hh <- fmap fromIntegral $ sampleDyn heightD
+      let batchTipHeight = fh + fromIntegral filtersBatchSize
+      restoreProgressWidget (filterStartingHeight BTC) fh hh
+      h3 $ localizedText RPSGetFiltsTitle
+      h4 $ localizedText $ RPSGetFiltsFromTo fh $ if batchTipHeight > hh then hh else batchTipHeight
+      psD <- getPubStorageD
       buildE <- getPostBuild
       let boolE = poke buildE $ const $ do
             h <- sampleDyn heightD
@@ -89,7 +95,7 @@ restorePage = wrapperSimple True $ void $ workflow nodeConnection
             int = repackKeys Internal $ pubStorageKeys BTC Internal ps
             keys = (V.++) ext int
             nextHeight = fh + fromIntegral (length batch)
-        pure $ scanBatchKeys nextHeight batch keys
+        pure $ scanBatchKeys (fh, nextHeight) batch keys
       finE <- delay 0.1 doneE
       let nextE = leftmost [finishScanning <$ finE, scanE]
       pure ((), nextE)
@@ -98,19 +104,23 @@ restorePage = wrapperSimple True $ void $ workflow nodeConnection
     -- Afterwards check if new keys are generated
     -- then feed them to stage 4 with the same batch of filters
     -- else go back to stage 3 with the nextHeight
-    scanBatchKeys :: BlockHeight -> [(BlockHeight, BlockHash, BtcAddrFilter)] -> V.Vector ScanKeyBox -> Workflow t m ()
-    scanBatchKeys nextHeight batch keys = Workflow $ do
+    scanBatchKeys :: (BlockHeight, BlockHeight)
+      -> [(BlockHeight, BlockHash, BtcAddrFilter)]
+      -> V.Vector ScanKeyBox
+      -> Workflow t m ()
+    scanBatchKeys (curHeight, nextHeight) batch keys = Workflow $ do
       (hE, cb) <- newTriggerEvent
-      hD <- holdDyn "" hE
-      h3 $ text "[scanBatchKeys] Scanning a batch ..."
-      h4 $ text $ "[scanBatchKeys] Next batch starts at " <> showt nextHeight
-      h4 $ dynText $ ffor hD $ \h -> "[scanBatchKeys] Scanning height: " <> h
+      hD <- holdDyn curHeight hE
+      hh <- sampleDyn . fmap fromIntegral =<< getCurrentHeight BTC
+      restoreProgressWidget (filterStartingHeight BTC) curHeight hh
+      h3 $ localizedText RPSScanTitle
+      h4 $ localizedDynText $ RPSScanProgress <$> hD
       let mkAddr k = addressToScriptBS . xPubToBtcAddr . extractXPubKeyFromEgv $ scanBox'key k
       let addrs = V.toList $ mkAddr <$> keys
       buildE <- getPostBuild
       let chunks = mkChunks 100 batch
       let foo (h, bh, filt) = do
-            liftIO $ cb $ showt h
+            liftIO $ cb h
             let bhash = egvBlockHashToHk bh
             res <- applyBtcFilterMany bhash filt addrs
             pure $ if res then Just (bhash, fromIntegral h) else Nothing
@@ -122,18 +132,19 @@ restorePage = wrapperSimple True $ void $ workflow nodeConnection
       keysE <- refreshBtcKeys $ void scanE
       let (nullE, extraE) = splitFilter V.null keysE
       goNewBatchE <- setScannedHeightE BTC $ nextHeight <$ nullE
-      let nextE = leftmost [getFiltersBatch <$ goNewBatchE, (scanBatchKeys nextHeight batch) <$> extraE]
+      let nextE = leftmost [getFiltersBatch <$ goNewBatchE, (scanBatchKeys (curHeight, nextHeight) batch) <$> extraE]
       pure ((), nextE)
 
     -- Stage 5: finalize the restore and exit to the balances page
     finishScanning = Workflow $ do
-      logWrite "Finished scanning BTC keys..."
       buildE <- getPostBuild
+      h2 $ localizedText RPSFinished
       setE <- setSyncProgress $ SyncProgress BTC Synced <$ buildE
-      void $ modifyPubStorage "finishScanning" $ ffor setE $ const $ \ps -> Just $ ps {
+      doneE <- modifyPubStorage "finishScanning" $ ffor setE $ const $ \ps -> Just $ ps {
           _pubStorage'restoring = False
         }
-      _ <- nextWidget $ ffor buildE $ const $ Retractable {
+      nextE <- delay 1 doneE
+      _ <- nextWidget $ ffor nextE $ const $ Retractable {
           retractableNext = balancesPage
         , retractablePrev = Nothing
         }
