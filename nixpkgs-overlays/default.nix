4f32b3c3bc82508557139223a25b509f4c7ee766
Anton Gushcha <ncrashed@protonmail.com>
2020-12-01 10:54:09 +0300

    Disable pyopenssl tests in nixpkgs overlay

--- nixpkgs-overlays/default.nix
+++ nixpkgs-overlays/default.nix
@@ -4,4 +4,11 @@ rec {
   secp256k1Sys = self.callPackage ../derivations/secp256k1Sys.nix {};
   zlibSys = self.callPackage ../derivations/zlibSys.nix {};
   lmdbSys = self.callPackage ../derivations/lmdbSys.nix {};
+  python = super.python.override {
+    packageOverrides = python-self: python-super: {
+      pyopenssl = python-super.pyopenssl.overrideAttrs (oldAttrs: {
+        doCheck = false;
+      });
+    };
+  };
 }
