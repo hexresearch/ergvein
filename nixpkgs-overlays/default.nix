c4a56fefaf981c983bf629653b64d9723471c7f1
Anton Gushcha <ncrashed@protonmail.com>
2020-12-02 16:54:14 +0300

    Disable tests for pyopenssl

--- nixpkgs-overlays/default.nix
+++ nixpkgs-overlays/default.nix
@@ -4,5 +4,8 @@ rec {
   secp256k1Sys = self.callPackage ../derivations/secp256k1Sys.nix {};
   zlibSys = self.callPackage ../derivations/zlibSys.nix {};
   lmdbSys = self.callPackage ../derivations/lmdbSys.nix {};
-  mercurial = null;
+
+  pyopenssl = super.pyopenssl.overridePythonAttrs(old: rec {
+    checkPhase = "";
+  });
 }
