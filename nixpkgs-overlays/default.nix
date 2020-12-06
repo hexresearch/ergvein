47980b5308abe11df57157648fedf11041368010
Anton Gushcha <ncrashed@protonmail.com>
2020-12-06 22:23:44 +0300

    Add leveldb to nixpkgs overlay. Fix #774

--- nixpkgs-overlays/default.nix
+++ nixpkgs-overlays/default.nix
@@ -5,4 +5,5 @@ in rec {
   secp256k1Sys = self.callPackage ../derivations/secp256k1Sys.nix {};
   zlibSys = self.callPackage ../derivations/zlibSys.nix {};
   lmdbSys = self.callPackage ../derivations/lmdbSys.nix {};
+  leveldb = self.callPackage ../derivations/leveldb.nix {};
 }
