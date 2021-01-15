0755d02cb0b8633c4caac6accda9b48dd7223da6
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2021-01-15 18:55:53 +0300

    Another variant of locking
    It just counts number of current transactions.

--- index-server/src/Ergvein/Index/Server/DB/Wrapper.hs
+++ index-server/src/Ergvein/Index/Server/DB/Wrapper.hs
@@ -17,7 +17,7 @@ module Ergvein.Index.Server.DB.Wrapper
   , streamSliceLDB
   ) where
 
-import Control.Concurrent.MVar
+import Control.Concurrent.STM
 import Control.Exception
 import Control.Monad.IO.Class
 import Data.Default
@@ -30,35 +30,52 @@ import qualified Database.LevelDB.Streaming as Stream
 -- | Connection to database. Actual connection is stored inside @MVar@
 -- and @Nothing@ is stored on when connection is closed. This way user
 -- won't be able to use now invalid connection,
-newtype LevelDB = LevelDB (MVar (Maybe DB))
+data LevelDB = LevelDB
+  { dbHandle :: TVar (Maybe DB) -- ^ Database handle.
+  , dbActive :: TVar Int        -- ^ Number of active DB operations
+  }
 
 -- | Open handle to a database
 openLevelDB :: MonadIO m => FilePath -> Options -> m LevelDB
 openLevelDB path opt = liftIO $ do
-  mv <- newEmptyMVar
-  db <- open path opt
-  putMVar mv (Just db)
-  pure $ LevelDB mv
+  dbActive <- newTVarIO 0
+  dbHandle <- newTVarIO . Just =<< open path opt
+  pure LevelDB{..}
 
 -- | Close and then immediately reopen database
 moveLevelDbHandle :: MonadIO m => LevelDB -> LevelDB -> m ()
-moveLevelDbHandle (LevelDB src) (LevelDB dst) = liftIO $ modifyMVar src $ \case
-  Nothing -> error "moveLevelDbHandle: can't move from closed handle"
-  Just h  -> modifyMVar dst $ \mh -> do
-    mapM_ unsafeClose mh
-    pure (Just h, (Nothing, ()))
+moveLevelDbHandle src dst = liftIO $ do
+  -- First close destination database
+  closeLevelDB dst
+  -- Move new one.
+  atomically $ do
+    writeTVar (dbHandle dst) =<< readTVar (dbHandle src)
+    writeTVar (dbActive dst) =<< readTVar (dbActive src)
+    writeTVar (dbHandle src) Nothing
+    writeTVar (dbActive src) 0
 
 -- | Close handle. After close it's no longer usable and any database
 -- operation which uses this handle will throw exception.
 closeLevelDB :: MonadIO m => LevelDB -> m ()
-closeLevelDB (LevelDB mv) = liftIO $ do
-  modifyMVar mv $ \mconn ->
-    (Nothing, ()) <$ mapM_ unsafeClose mconn
+-- FIXME: Masking???
+closeLevelDB LevelDB{..} = liftIO $ do
+  -- First mark database as closed so no new transaction could be started
+  mdb <- atomically $ readTVar dbHandle <* writeTVar dbHandle Nothing
+  case mdb of
+    Nothing -> pure ()
+    Just db -> do
+      -- Wait until all requests are finished then close database
+      atomically $ check . (==0) =<< readTVar dbActive
+      unsafeClose db
+
 
 usingLevelDB :: MonadIO m => LevelDB -> (DB -> IO a) -> m a
-usingLevelDB (LevelDB h) action = liftIO $ withMVar h $ \case
-  Nothing -> error "usingLevelDB: database is already closed"
-  Just c  -> action c
+usingLevelDB LevelDB{..} = liftIO . bracket ini fini
+  where
+    ini = atomically $ readTVar dbHandle >>= \case
+      Nothing -> error "LevelDB: Database is closed"
+      Just db -> db <$ modifyTVar' dbActive succ
+    fini _ = atomically $ modifyTVar' dbActive pred
 
 
 ----------------------------------------------------------------
