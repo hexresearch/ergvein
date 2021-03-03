bdb56a56955203879ac9b15ff13f30bca3a99750
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2021-03-03 13:55:34 +0300

    +++

--- ergo-protocol-client/app/Main.hs
+++ ergo-protocol-client/app/Main.hs
@@ -45,11 +45,25 @@ main = do
         t <- getCurrentTime
         atomically $ writeTChan inChan $ SockInSendEvent $ MsgHandshake $ makeHandshake 0 t
         threadDelay 1000000
-        -- let requiredBlock = "8cf6dca6b9505243e36192fa107735024c0000cf4594b1daa2dc4e13ee86f26f"
-        -- atomically $ writeTChan inChan $ SockInSendEvent $ MsgOther $ MsgRequestModifier $ RequestModifierMsg ModifierBlockHeader [requiredBlock]
-        atomically $ writeTChan inChan $ SockInSendEvent $ MsgOther $ MsgSyncInfo $ SyncInfo [nullModifierId]
+        let --requiredBlock = "8cf6dca6b9505243e36192fa107735024c0000cf4594b1daa2dc4e13ee86f26f"
+            requiredBlock = "b21a1c00412b84033185f3cf6cdd345c4276628f3dda1e63b8502a4923c8e2bc"
+            -- ty = ModifierBlockHeader
+            ty = ModifierBlockTxs
+        atomically $ writeTChan inChan $ SockInSendEvent $ MsgOther $ MsgRequestModifier $ RequestModifierMsg ty [requiredBlock]
+        -- atomically $ writeTChan inChan $ SockInSendEvent $ MsgOther $ MsgSyncInfo $ SyncInfo [nullModifierId]
       SockOutInbound (MsgOther (MsgInv (InvMsg itype is))) -> do
         atomically $ writeTChan inChan $ SockInSendEvent $ MsgOther $ MsgRequestModifier $ RequestModifierMsg itype $ V.singleton $ V.head is
         pure ()
       _ -> pure ()
-    print ev
+    case ev of
+      SockOutInbound (MsgHandshake h) -> putStrLn "HANDSHAKE" >> print h
+      SockOutInbound (MsgOther msg) -> case msg of
+        MsgInv      _ -> putStrLn "MsgInv"
+        MsgSyncInfo (SyncInfo bids) -> do
+          putStrLn "MsgSyncInfo"
+          -- mapM_ print bids
+        MsgRequestModifier _ -> putStrLn "MsgRequestModifier"
+        MsgModifier m -> do
+          putStrLn "MsgModifier"
+          print m
+      _ -> print ev
