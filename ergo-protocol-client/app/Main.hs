caad5c485a6ccbbb317ed4af23646831ab4a4a77
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2021-03-04 16:22:00 +0300

    ++

--- ergo-protocol-client/app/Main.hs
+++ ergo-protocol-client/app/Main.hs
@@ -1,4 +1,5 @@
-{-# LANGUAGE OverloadedLists #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE OverloadedLists       #-}
 module Main where
 
 import Control.Concurrent
@@ -7,10 +8,12 @@ import Control.Monad
 import Data.Ergo.Modifier
 import Data.Ergo.Protocol
 import Data.Ergo.Protocol.Client
+import Data.Ergo.Block (BlockHeader)
 import Data.Maybe
+import Data.Persist
 import Data.Time
 import Options.Generic
-
+import Data.Vector.Generic ((!))
 import qualified Data.Vector as V
 
 data Options = Options {
@@ -46,9 +49,11 @@ main = do
         atomically $ writeTChan inChan $ SockInSendEvent $ MsgHandshake $ makeHandshake 0 t
         threadDelay 1000000
         let --requiredBlock = "8cf6dca6b9505243e36192fa107735024c0000cf4594b1daa2dc4e13ee86f26f"
-            requiredBlock = "b21a1c00412b84033185f3cf6cdd345c4276628f3dda1e63b8502a4923c8e2bc"
+            -- requiredBlock = "b21a1c00412b84033185f3cf6cdd345c4276628f3dda1e63b8502a4923c8e2bc"
+            requiredBlock = "30075a8396918d62ba187d7e32cf3623e393f0ac1881e221bc5fb9515de633d0"
             -- ty = ModifierBlockHeader
-            ty = ModifierBlockTxs
+            -- ty = ModifierBlockTxs
+            ty = UnknownModifier 1
         atomically $ writeTChan inChan $ SockInSendEvent $ MsgOther $ MsgRequestModifier $ RequestModifierMsg ty [requiredBlock]
         -- atomically $ writeTChan inChan $ SockInSendEvent $ MsgOther $ MsgSyncInfo $ SyncInfo [nullModifierId]
       SockOutInbound (MsgOther (MsgInv (InvMsg itype is))) -> do
@@ -63,7 +68,31 @@ main = do
           putStrLn "MsgSyncInfo"
           -- mapM_ print bids
         MsgRequestModifier _ -> putStrLn "MsgRequestModifier"
-        MsgModifier m -> do
+        MsgModifier (ModifierMsg ty m) -> do
           putStrLn "MsgModifier"
-          print m
+          print ty
+          let bs = modifierBody $ m ! 0
+          print $ bs
+          print $ decode @BlockHeader bs
       _ -> print ev
+
+{- HEADER
+q = BlockHeader
+  { version          = 1
+  , parentId         = cee668bdcb8d24cc25569e82d7500b2c56eefddfa4629834de1cf6c96b2bfc80
+  , adProofsRoot     = 47bfb6efa2ed041e51013e905f045ebf5af19e1a8510a98a516d355d31116908
+  , transactionsRoot = a191856a3fd7703a92b8f8c397b8a0100195e98f408654e419ed01d03fc5f959
+  , stateRoot        = d2ab2775e281381451e114c2c6fd84c6aa2acc9f349575387dbcfb79a8e57ffc13
+  , timestamp        = 2021-02-02 14:20:55.509 UTC
+  , extensionRoot    = 30075a8396918d62ba187d7e32cf3623e393f0ac1881e221bc5fb9515de633d0
+  , nBits            = Difficulty {unDifficulty = 3381663975342080}
+  , height           = 417791
+  , votes            = ParamVotes 4 3 0
+  , powSolution = AutolykosSolution
+    { minerPubKey = 030e9662f3ed3448512424a273b3820ef83d3fee593cda88a115f344dc76ec4322
+    , oneTimePubKey = 038036568285bbb106e3c3fe5a9a333b3663a02c56021f1eeb7222a85808365578
+    , nonce = "\NUL\NUL\SO;\SOH\140\SO\201"
+    , distance = 2642678796460668869164530876036253368923823090434204406855695
+    }
+  }
+-}
