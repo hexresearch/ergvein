6a411d943f0b4cb23f6f513b87230886ed391b37
Aleksey Khudyakov <alexey.skladnoy@gmail.com>
2021-03-22 13:59:13 +0300

    Don't leak thread in Ergvein.Index.Server.TCPService.BTC (#890)
    * Block. Don't poll
    
    * Read from 2 TChans
    
    * Move code for thread supervision to separate module
    
    * Use withLinkedWorker_ to avoid thread leak
    
    * Drop another thread by using <|> for STM
    
    * Kill worker threads when main thread is done
    
    Also rely on usual bracket for closing socket.
    
    * Use forkIO since we're working in IO anyway

--- index-server/src/Ergvein/Index/Server/TCPService/Server.hs
+++ index-server/src/Ergvein/Index/Server/TCPService/Server.hs
@@ -2,14 +2,9 @@
 module Ergvein.Index.Server.TCPService.Server where
 
 import Control.Applicative
-import Control.Concurrent
-import Control.Concurrent.Async.Lifted (Async,async)
 import Control.Concurrent.STM
 import Control.Immortal                (Thread,create,stop)
-import Control.Exception               (AsyncException(..),throwIO)
 import Control.Monad
-import Control.Monad.Base
-import Control.Monad.Trans.Control
 import Control.Monad.Catch
 import Control.Monad.IO.Unlift
 import Control.Monad.Logger
@@ -19,7 +14,6 @@ import qualified Control.Concurrent.Async as Async
 import Data.Attoparsec.ByteString
 import Data.ByteString.Builder
 import Data.Either.Combinators
-import Data.Set (Set)
 import Data.Word
 import Network.Socket
 
@@ -34,77 +28,10 @@ import Ergvein.Index.Server.Metrics
 import Ergvein.Index.Server.Monad
 import Ergvein.Index.Server.TCPService.MessageHandler
 import Ergvein.Index.Server.TCPService.Socket as S
+import Ergvein.Index.Server.TCPService.Supervisor
 
 import qualified Data.ByteString as BS
-import qualified Data.Set        as Set
 import qualified Network.Socket.ByteString as NS
-
-
-----------------------------------------------------------------
--- Async stuff
-----------------------------------------------------------------
-
--- | Unhandled exception in child thread.
-data ExceptionInLinkedThread = ExceptionInLinkedThread SomeException
-  deriving Show
-instance Exception ExceptionInLinkedThread
-
--- | Create worker thread
-withLinkedWorker
-  :: (MonadBaseControl IO m)
-  => m a -> (Async () -> m b) -> m b
-withLinkedWorker action cont = restoreM =<< do
-  -- FIXME: test that we correctly deal with blocking calls with
-  --        throwTo. async use uninterruptibleCancel for example.
-  liftBaseWith $ \runInIO -> do
-    tid <- myThreadId
-    mask $ \restore -> do
-      -- Here we spawn worker thread which will throw unhandled exception to main thread.
-      a <- Async.async $ restore (runInIO action) `catch` \e -> do
-        unless (ignoreException e) $ throwTo tid (ExceptionInLinkedThread e)
-        throwIO e
-      restore (runInIO (cont (() <$ a))) `finally` Async.cancel a
-
--- | Same as 'withLinkedWorker' for use in cases when
-withLinkedWorker_ :: (MonadBaseControl IO m) => m a -> m b -> m b
-withLinkedWorker_ action = withLinkedWorker action . const
-
--- Exception to ignore for linked threads
-ignoreException :: SomeException -> Bool
-ignoreException e
-  | Just Async.AsyncCancelled <- fromException e = True
-  | Just ThreadKilled         <- fromException e = True
-  | otherwise = False
-
-
--- | Set of worker threads
-newtype WorkersUnion = WorkersUnion (TVar (Set ThreadId))
-
--- | Create handler for set of worker threads which all will be
--- limited once 'withWorkersUnion' terminates.
-withWorkersUnion
-  :: (MonadIO m, MonadMask m)
-  => (WorkersUnion -> m a) -> m a
-withWorkersUnion = bracket ini fini
-  where
-    ini  = WorkersUnion <$> liftIO (newTVarIO mempty)
-    fini (WorkersUnion tidsVar) = liftIO $ do
-      tids <- readTVarIO tidsVar
-      forM_ tids $ \tid -> forkIO $ throwTo tid Async.AsyncCancelled
-
--- | Spawn worker thread which will be terminated when
--- 'withWorkersUnion' exits.
-spawnWorker
-  :: (MonadBaseControl IO m, MonadMask m)
-  => WorkersUnion -> m a -> m ()
-spawnWorker (WorkersUnion tidsVar) action = do
-  mask_ $ do
-    a <- async action
-    liftBase $ atomically $ modifyTVar' tidsVar $ Set.insert (Async.asyncThreadId a)
-
-----------------------------------------------------------------
--- Server
-----------------------------------------------------------------
 
 
 runTcpSrv :: ServerM Thread
