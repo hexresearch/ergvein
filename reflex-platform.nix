9cdc6b43fa13df395c5ebd0e6c854721cae3a229
Anton Gushcha <ncrashed@protonmail.com>
2021-03-27 13:45:42 +0300

    Update reflex platform

--- reflex-platform.nix
+++ reflex-platform.nix
@@ -2,7 +2,7 @@
 import ((import <nixpkgs> {}).fetchFromGitHub {
   owner = "hexresearch";
   repo = "reflex-platform";
-  rev = "f76ff088aa0af677e7f216391690d7df37341ee2";
-  sha256  = "15f6szr0fl5q0h10g7vpmc3s93aipb5wmy9mrkslqxz9h0rbcqzh";
+  rev = "51dd4ef19141d055daee63e1ee8736dec5037ae7";
+  sha256  = "0fb0kaa31bnr1m326660vwrpmmyha3831kbyvw0a8v68ggy4kx8i";
 })
 /* import ../reflex-platform  */
