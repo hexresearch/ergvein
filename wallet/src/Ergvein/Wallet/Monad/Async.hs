894cacdc5558de656f441379b89a8fdf5728c6a9
Alexey Khudyakov <alexey.skladnoy@gmail.com>
2021-03-30 20:48:44 +0300

    Add withLinkedWorker variant which uses forkOn internally
    We have to define it here since we need to plug correct async and we use
    MonadUnliftIO instead of MonadBaseControl since rest of code here uses unliftio.
    We really need some sort of standadization

--- wallet/src/Ergvein/Wallet/Monad/Async.hs
+++ wallet/src/Ergvein/Wallet/Monad/Async.hs
@@ -7,12 +7,14 @@ module Ergvein.Wallet.Monad.Async(
   ) where
 
 import Control.Concurrent
+import Control.Concurrent.Async hiding (ExceptionInLinkedThread(..))
 import Control.Exception
 import Control.Monad.IO.Class
 import Control.Monad.IO.Unlift
 import Control.Monad.Random.Strict
 import Ergvein.Text
 import Ergvein.Wallet.Native
+import Ergvein.Concurrent (ExceptionInLinkedThread(..), defaultIgnoredExceptions)
 import Foreign.JavaScript.TH (WithJSContextSingleton(..))
 import Reflex.Spider.Internal (SpiderHostFrame(..), EventM(..))
 import Reflex
@@ -31,13 +33,30 @@ bindSelf io = do
 
 -- | Fork new thread and bind it to other capability than current thread.
 forkOnOther :: IO () -> IO ThreadId
-forkOnOther m = do
+forkOnOther m = capabilityToForkOn >>= \case
+  Nothing -> forkIO m
+  Just i  -> forkOn i m
+
+withLinkedWorkerOnOther :: (MonadUnliftIO m) => m a -> m b -> m b
+withLinkedWorkerOnOther action cont = do
+  unlift <- askUnliftIO
+  liftIO $ do
+    tid     <- myThreadId
+    doAsync <- maybe async asyncOn <$> capabilityToForkOn
+    mask $ \restore -> do
+      -- Here we spawn worker thread which will throw unhandled exception to main thread.
+      a <- doAsync $ unliftIO unlift action `catch` \e -> do
+        unless (defaultIgnoredExceptions e) $ throwTo tid (ExceptionInLinkedThread e)
+        throwIO e
+      unliftIO unlift cont `finally` cancel a
+
+capabilityToForkOn :: IO (Maybe Int)
+capabilityToForkOn = do
   (cap, _) <- threadCapability =<< myThreadId
   getNumCapabilities >>= \case
-    1 -> forkIO m
-    n -> do i <- uniform [i | i <- [0 .. n-1], i /= cap]
-            -- putStrLn $ "Our cap is " ++ show cap ++ ", forking on " ++ show i
-            forkOn i m
+    1 -> pure Nothing
+    n -> Just <$> uniform [i | i <- [0 .. n-1], i /= cap]
+
 
 -- | Helper that runs action in event in new thread with respect for logging of errors.
 performFork
@@ -58,6 +77,7 @@ performFork_ em = performEvent_ $ ffor em $ \ma -> do
   void . liftIO . forkOnOther $ do
     ea :: Either SomeException () <- try $ unliftIO unlift ma
     either (logWrite . ("Forked event failed: " <>) . showt) pure ea
+
 
 instance MonadUnliftIO m => MonadUnliftIO (WithJSContextSingleton x m) where
   withRunInIO = wrappedWithRunInIO WithJSContextSingleton unWithJSContextSingleton
