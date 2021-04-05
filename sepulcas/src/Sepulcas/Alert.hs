dfa2d36bd21dbe2afb362924e2cc6bfac7e1d456
Anton Gushcha <ncrashed@protonmail.com>
2021-04-05 13:11:52 +0300

    Add Sepulcas.Alert.Handler

--- sepulcas/src/Sepulcas/Alert.hs
+++ sepulcas/src/Sepulcas/Alert.hs
@@ -1,7 +1,9 @@
 module Sepulcas.Alert(
-    module Sepulcas.Alert.Types
+    module Sepulcas.Alert.Handler
+  , module Sepulcas.Alert.Types
   , module Sepulcas.Alert.Monad
   ) where
 
-import Sepulcas.Alert.Types
+import Sepulcas.Alert.Handler
 import Sepulcas.Alert.Monad
+import Sepulcas.Alert.Types
