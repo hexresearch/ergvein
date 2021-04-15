5c59ac4ae32e50f7438afc28daad1c554e9d5431
NCrashed <ncrashed@protonmail.com>
2021-04-15 18:04:36 +0300

    Move indexer socket

--- ergvein-core/src/Ergvein/Core/Client.hs
+++ ergvein-core/src/Ergvein/Core/Client.hs
@@ -1,5 +1,7 @@
 module Ergvein.Core.Client(
     module Ergvein.Core.Client.Monad
+  , module Ergvein.Core.Client.Socket
   ) where
 
 import Ergvein.Core.Client.Monad
+import Ergvein.Core.Client.Socket
