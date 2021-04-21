b90999143ba29ecd87fa77cd019a069e4f331fe6
NCrashed <ncrashed@protonmail.com>
2021-04-22 00:08:45 +0300

    Implement Unauth env

---
+++ ergvein-core/src/Ergvein/Core/Env/Auth.hs
@@ -0,0 +1,3 @@
+module Ergvein.Core.Env.Auth(
+
+  ) where
---
+++ ergvein-core/src/Ergvein/Core/Env/Unauth.hs
@@ -0,0 +1,43 @@
+module Ergvein.Core.Env.Unauth(
+    UnauthEnv(..)
+  , UnauthM
+  , newUnauthEnv
+  , runUnauth
+  ) where
+
+import Control.Monad.Reader
+import Ergvein.Core.Client
+import Ergvein.Core.Settings
+import Ergvein.Core.Wallet
+import Reflex.Main.Thread
+
+data UnauthEnv t = UnauthEnv {
+  unauth'settings :: !(SettingsEnv t)
+, unauth'wallet   :: !(PreWalletEnv t)
+, unauth'client   :: !(ClientEnv t)
+}
+
+type UnauthM t m = ReaderT (UnauthEnv t) m
+
+instance Monad m => HasSettingsEnv t (UnauthM t m) where
+  getSettingsEnv = asks unauth'settings
+  {-# INLINE getSettingsEnv #-}
+
+instance Monad m => HasPreWalletEnv t (UnauthM t m) where
+  getPreWalletEnv = asks unauth'wallet
+  {-# INLINE getPreWalletEnv #-}
+
+instance Monad m => HasClientEnv t (UnauthM t m) where
+  getClientEnv = asks unauth'client
+  {-# INLINE getClientEnv #-}
+
+newUnauthEnv :: (MonadIO m, TriggerEvent t m, MonadHasMain m, MonadSettingsConstr t m) => Settings -> m (UnauthEnv t)
+newUnauthEnv settings = do
+  senv <- newSettingsEnv settings
+  UnauthEnv
+    <$> pure senv
+    <*> newPreWalletEnv
+    <*> runReaderT newClientEnv senv
+
+runUnauth :: UnauthEnv t -> UnauthM t m a -> m a
+runUnauth = flip runReaderT
