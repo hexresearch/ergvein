600fe54cd65c512969b92b23da7cc4b971d87cb0
NCrashed <ncrashed@protonmail.com>
2021-05-31 18:51:00 +0300

    Add transaction decoder module

--- ergo-protocol/src/Data/Ergo/Transaction.hs
+++ ergo-protocol/src/Data/Ergo/Transaction.hs
@@ -1,6 +1,8 @@
 module Data.Ergo.Transaction
-  ( module Data.Ergo.Transaction.Types
+  ( module Data.Ergo.Transaction.Decoder
+  , module Data.Ergo.Transaction.Types
   )
 where
 
+import Data.Ergo.Transaction.Decoder
 import Data.Ergo.Transaction.Types
---
+++ ergo-protocol/src/Data/Ergo/Transaction/Decoder.hs
@@ -0,0 +1,3 @@
+module Data.Ergo.Transaction.Decoder(
+
+  ) where
