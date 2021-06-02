0491e0ff21bf269a6b8833a2964c83256ec2c429
fastpok <krutkinvs@gmail.com>
2021-06-03 00:32:26 +0300

    Ergo send page WIP

--- wallet/src/Ergvein/Wallet/Widget/Input/Amount.hs
+++ wallet/src/Ergvein/Wallet/Widget/Input/Amount.hs
@@ -1,7 +1,8 @@
 {-# OPTIONS_GHC -Wall #-}
 
 module Ergvein.Wallet.Widget.Input.Amount(
-    sendAmountWidget
+      sendAmountWidgetBtc
+    , sendAmountWidgetErg
   ) where
 
 import Control.Monad.Except
@@ -9,60 +10,92 @@ import Data.Maybe
 import Data.Word
 
 import Ergvein.Either
-import Sepulcas.Elements
 import Ergvein.Wallet.Language
 import Ergvein.Wallet.Localize
 import Ergvein.Wallet.Monad
+import Ergvein.Wallet.Settings
 import Ergvein.Wallet.Validate
 import Ergvein.Wallet.Widget.Balance
+import Sepulcas.Elements
+import Sepulcas.Text (Display(..))
 
 import qualified Data.Map.Strict as M
-import qualified Data.Text as T
 
--- | Input field with units. Converts everything to satoshis and returns the unit
-sendAmountWidget = undefined
--- sendAmountWidget :: MonadFront t m => Maybe (UnitBTC, Word64) -> Event t () -> m (Dynamic t (Maybe (UnitBTC, Word64)))
--- sendAmountWidget minit submitE = divClass "amoun-input" $ mdo
---   setUs <- fmap (fromMaybe defUnits . settingsUnits) getSettings
---   let (unitInit, txtInit) = maybe (setUs, "") (\(u, a) -> let us = Units (Just u) Nothing
---         in (us, showMoneyUnit (Money BTC a) us)) minit
---   let errsD = fmap (maybe [] id) amountErrsD
---   let isInvalidD = fmap (maybe "" (const "is-invalid")) amountErrsD
---   amountValD <- do
---     el "label" $ localizedText AmountString
---     divClass "row" $ mdo
---       textInputValueD <- divClass "column column-67" $ do
---         textInputValueD' <- (fmap . fmap) T.unpack $ divClassDyn isInvalidD $ textFieldAttrNoLabel (M.singleton "class" "mb-0") never never txtInit
---         when isAndroid (availableBalanceWidget unitD)
---         pure textInputValueD'
---       unitD <- divClass "column column-33" $ do
---         unitsDropdown (getUnitBTC unitInit) allUnitsBTC
---       pure $ zipDynWith (\u v -> fmap (u,) $ toEither $ validateBtcWithUnits u v) unitD textInputValueD
---   void $ divClass "form-field-errors" $ simpleList errsD displayError
---   amountErrsD :: Dynamic t (Maybe [VError ()]) <- holdDyn Nothing $ ffor (current amountValD `tag` submitE) eitherToMaybe'
---   pure $ eitherToMaybe <$> amountValD
---   where
---     availableBalanceWidget uD = do
---       balanceValue <- balancesWidget BTC
---       balanceText <- localized SendAvailableBalance
---       let balanceVal = zipDynWith (\x y -> showMoneyUnit x (Units (Just y) Nothing) <> " " <> btcSymbolUnit y) balanceValue uD
---           balanceTxt = zipDynWith (\x y -> x <> ": " <> y) balanceText balanceVal
---       divClass "amount-available-balance" $ dynText balanceTxt
---     unitsDropdown val allUnits = do
---       langD <- getLanguage
---       let unitD = constDyn val
---       initKey <- sample . current $ unitD
---       let listUnitsD = ffor langD $ \l -> M.fromList $ fmap (\v -> (v, localizedShow l v)) allUnits
---           ddnCfg = DropdownConfig {
---               _dropdownConfig_setValue = updated unitD
---             , _dropdownConfig_attributes = constDyn ("class" =: "mb-0")
---             }
---       dp <- dropdown initKey listUnitsD ddnCfg
---       let selD = _dropdown_value dp
---       holdUniqDyn selD
---     displayError :: (MonadFrontBase t m, LocalizedPrint l) => Dynamic t l -> m ()
---     displayError errD = do
---       langD <- getLanguage
---       let localizedErrD = zipDynWith localizedShow langD errD
---       dynText localizedErrD
---       br
+availableBalanceWidget :: (MonadFront t m, IsMoneyUnit a, Display a) => Currency -> Dynamic t a -> m ()
+availableBalanceWidget cur uD = do
+  balanceValue <- balancesWidget cur
+  balanceTitle <- localized SendAvailableBalance
+  let balanceVal = zipDynWith (\x y -> showMoneyUnit x y <> " " <> display y) balanceValue uD
+      balanceText = zipDynWith (\x y -> x <> ": " <> y) balanceTitle balanceVal
+  divClass "amount-available-balance" $ dynText balanceText
+
+unitsDropdown :: (MonadFront t m, Ord a, LocalizedPrint a) => a -> [a] -> m (Dynamic t a)
+unitsDropdown initialUnit allUnits = do
+  langD <- getLanguage
+  let listUnitsD = ffor langD $ \l -> M.fromList $ fmap (\v -> (v, localizedShow l v)) allUnits
+      config = DropdownConfig {
+            _dropdownConfig_setValue = never
+          , _dropdownConfig_attributes = constDyn ("class" =: "mb-0")
+        }
+  dp <- dropdown initialUnit listUnitsD config
+  let selD = _dropdown_value dp
+  holdUniqDyn selD
+
+displayError :: (MonadFrontBase t m, LocalizedPrint l) => Dynamic t l -> m ()
+displayError errD = do
+  langD <- getLanguage
+  let localizedErrD = zipDynWith localizedShow langD errD
+  dynText localizedErrD
+  br
+
+-- | Input field with units. Converts everything to satoshis and returns the unit.
+sendAmountWidgetBtc :: (MonadFront t m) => Maybe (UnitBTC, Word64) -> Event t () -> m (Dynamic t (Maybe (UnitBTC, Word64)))
+sendAmountWidgetBtc minit submitE = divClass "amount-input" $ mdo
+  let errsD = fromMaybe [] <$> amountErrsD
+      isInvalidD = fmap (maybe "" (const "is-invalid")) amountErrsD
+  amountValD <- do
+    el "label" $ localizedText AmountString
+    divClass "row" $ mdo
+      textInputValueD <- divClass "column column-67" $ do
+        textInputValueD' <- do
+          txtInit <- do
+            units <- getSettingsUnitBtc
+            let unitInit = maybe units fst minit
+            pure $ maybe "" (\(_, amount) -> showMoneyUnit (Money BTC amount) unitInit) minit
+          divClassDyn isInvalidD $ textFieldAttrNoLabel (M.singleton "class" "mb-0") never never txtInit
+        when isAndroid (availableBalanceWidget BTC unitD)
+        pure textInputValueD'
+      unitD <- divClass "column column-33" $ do
+          units <- getSettingsUnitBtc
+          let unitInit = maybe units fst minit
+          unitsDropdown unitInit allUnitsBTC
+      pure $ zipDynWith (\u v -> fmap (u,) $ toEither $ validateAmount 0 u v) unitD textInputValueD
+  void $ divClass "form-field-errors" $ simpleList errsD displayError
+  amountErrsD <- holdDyn Nothing $ ffor (current amountValD `tag` submitE) eitherToMaybe'
+  pure $ eitherToMaybe <$> amountValD
+
+-- | Input field with units. Converts everything to satoshis and returns the unit.
+sendAmountWidgetErg :: (MonadFront t m) => Maybe (UnitERGO, Word64) -> Event t () -> m (Dynamic t (Maybe (UnitERGO, Word64)))
+sendAmountWidgetErg minit submitE = divClass "amount-input" $ mdo
+  let errsD = fromMaybe [] <$> amountErrsD
+      isInvalidD = fmap (maybe "" (const "is-invalid")) amountErrsD
+  amountValD <- do
+    el "label" $ localizedText AmountString
+    divClass "row" $ mdo
+      textInputValueD <- divClass "column column-67" $ do
+        textInputValueD' <- do
+          txtInit <- do
+            units <- getSettingsUnitErg
+            let unitInit = maybe units fst minit
+            pure $ maybe "" (\(_, amount) -> showMoneyUnit (Money ERGO amount) unitInit) minit
+          divClassDyn isInvalidD $ textFieldAttrNoLabel (M.singleton "class" "mb-0") never never txtInit
+        when isAndroid (availableBalanceWidget ERGO unitD)
+        pure textInputValueD'
+      unitD <- divClass "column column-33" $ do
+          units <- getSettingsUnitErg
+          let unitInit = maybe units fst minit
+          unitsDropdown unitInit allUnitsERGO
+      pure $ zipDynWith (\u v -> fmap (u,) $ toEither $ validateAmount 0 u v) unitD textInputValueD
+  void $ divClass "form-field-errors" $ simpleList errsD displayError
+  amountErrsD <- holdDyn Nothing $ ffor (current amountValD `tag` submitE) eitherToMaybe'
+  pure $ eitherToMaybe <$> amountValD
