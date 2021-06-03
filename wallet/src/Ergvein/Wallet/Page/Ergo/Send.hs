ac0bd304d4cb1300e4745a2631a2e138bee10a06
fastpok <krutkinvs@gmail.com>
2021-06-03 21:38:35 +0300

    Ergo send page WIP

--- wallet/src/Ergvein/Wallet/Page/Ergo/Send.hs
+++ wallet/src/Ergvein/Wallet/Page/Ergo/Send.hs
@@ -1,4 +1,4 @@
-{-# OPTIONS_GHC -Wall #-}
+-- {-# OPTIONS_GHC -Wall #-}
 
 module Ergvein.Wallet.Page.Ergo.Send (
     sendPageErg
@@ -12,11 +12,14 @@ import Ergvein.Wallet.Monad
 import Ergvein.Wallet.Navbar
 import Ergvein.Wallet.Navbar.Types
 import Ergvein.Wallet.Orphanage ()
+import Ergvein.Wallet.Page.Balances
 import Ergvein.Wallet.Widget.Input.Amount
 import Ergvein.Wallet.Widget.Input.Fee
 import Ergvein.Wallet.Widget.Input.Recipient
 import Ergvein.Wallet.Wrapper
 import Sepulcas.Elements
+
+import qualified Data.ByteString as B
 
 sendPageErg :: MonadFront t m => Maybe ((UnitERGO, Word64), (FeeMode, Word64), ErgAddress) -> m ()
 sendPageErg mInit = mdo
@@ -58,4 +61,36 @@ sendWidget mInit title navbar thisWidget = wrapperNavbar False title thisWidget 
 
 -- | Main confirmation & sign & send widget
 sendConfirmationWidget :: MonadFront t m => ((UnitERGO, Word64), Word64, ErgAddress) -> m ()
-sendConfirmationWidget = undefined
+sendConfirmationWidget v = do
+  walletName <- getWalletName
+  title <- localized walletName
+  let thisWidget = Just $ pure $ sendConfirmationWidget v
+      navbar = if isAndroid
+        then blank
+        else navbarWidget ERGO thisWidget NavbarSend
+  wrapperNavbar False title thisWidget navbar $ divClass "send-confirm-box" $ mdo
+    stxE <- makeTxWidget v
+    void $ networkHold (pure ()) $ ffor stxE $ \tx -> do
+      sendE <- getPostBuild
+      addedE <- addOutgoingTx "sendConfirmationWidget" $ TxErg (ErgTx tx Nothing) <$ sendE
+      -- save tx in storage
+      -- storedE <- btcMempoolTxInserter $ tx <$ addedE
+
+      -- broadcast tx
+      -- void $ requestBroadcast $ ffor storedE $ const $
+      --   NodeReqBtc . MInv . Inv . pure . InvVector InvTx . HT.getTxHash . HT.txHash $ tx
+      goE <- delay 1 =<< outlineButton SendBtnBack
+      void $ nextWidget $ ffor goE $ const $ Retractable {
+            retractableNext = balancesPage
+          , retractablePrev = Nothing
+        }
+
+mockErgoTx :: ErgTxRaw
+mockErgoTx = ErgTransaction B.empty
+
+makeTxWidget :: MonadFront t m =>
+  ((UnitERGO, Word64), Word64, ErgAddress) ->
+  m (Event t ErgTxRaw)
+makeTxWidget ((unit, amount), fee, addr) = do
+  buildE <- getPostBuild
+  pure $ mockErgoTx <$ buildE
