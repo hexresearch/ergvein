f601b446e82a0399d7327c4447a5952c44992569
fastpok <krutkinvs@gmail.com>
2021-06-21 19:18:21 +0300

    Added ErgoBox data type and renamed ErgoBoxCandidate fields

--- ergo-protocol/src/Data/Ergo/Transaction.hs
+++ ergo-protocol/src/Data/Ergo/Transaction.hs
@@ -3,6 +3,7 @@ module Data.Ergo.Transaction
     , TxDataInput (..)
     , Token (..)
     , Transaction (..)
+    , ErgoBox (..)
     , ErgoBoxCandidate (..)
   )
 where
@@ -11,7 +12,7 @@ import Data.ByteString (ByteString)
 import Data.Ergo.Block (Digest32 (Digest32))
 import Data.Map (Map)
 import Data.Persist
-import Data.Word (Word32, Word64)
+import Data.Word (Word16, Word32, Word64)
 
 data TxInput = TxInput
   { -- | Id of the box to spent
@@ -29,9 +30,46 @@ newtype TxDataInput = TxDataInput
 
 data Token = Token
   { -- Token id
-    tokenId :: Digest32,
+    tokenId :: !Digest32,
     -- Token amount
-    tokenAmount :: Word64
+    tokenAmount :: !Word64
+  }
+  deriving (Show, Read, Eq)
+
+-- Box (aka coin, or an unspent output) is a basic concept of a UTXO-based cryptocurrency.
+-- In Bitcoin, such an object is associated with some monetary value (arbitrary,
+-- but with predefined precision, so we use integer arithmetic to work with the value),
+-- and also a guarding script (aka proposition) to protect the box from unauthorized opening.
+--
+-- In other way, a box is a state element locked by some proposition (ErgoTree).
+--
+-- In Ergo, box is just a collection of registers, some with mandatory types and semantics,
+-- others could be used by applications in any way.
+-- We add additional fields in addition to amount and proposition~(which stored in the registers R0 and R1).
+-- Namely, register R2 contains additional tokens (a sequence of pairs (token identifier, value)).
+-- Register R3 contains height specified by user (protocol checks if it was <= current height when
+-- transaction was accepted) and also transaction identifier and box index in the transaction outputs.
+-- Registers R4-R9 are free for arbitrary usage.
+--
+-- A transaction is unsealing a box. As a box can not be open twice, any further valid transaction
+-- can not be linked to the same box.
+data ErgoBox = ErgoBox
+  { -- | Amount of money associated with the box
+    ergoBox'boxValue :: !Word64,
+    -- | Guarding script, which should be evaluated to true in order to open this box
+    ergoBox'ergoTree :: !ByteString,
+    -- | Secondary tokens the box contains
+    ergoBox'tokens :: ![Token],
+    -- | Additional registers the box can carry over
+    ergoBox'additionalRegisters :: !ByteString,
+    -- | Height when a transaction containing the box was created.
+    -- This height is declared by user and should not exceed height of the block,
+    -- containing the transaction with this box.
+    ergoBox'creationHeight :: !Word32,
+    -- | Id of transaction which created the box
+    ergoBox'transactionId :: !Digest32,
+    -- | Number of box (from 0 to total number of boxes the transaction with transactionId created - 1)
+    ergoBox'index :: !Word16
   }
   deriving (Show, Read, Eq)
 
@@ -39,17 +77,17 @@ data Token = Token
 -- that will be calculated after full transaction formation.
 data ErgoBoxCandidate = ErgoBoxCandidate
   { -- | Amount of money associated with the box
-    boxValue :: !Word64,
+    ergoBoxCandidate'boxValue :: !Word64,
     -- | Guarding script, which should be evaluated to true in order to open this box
-    ergoTree :: !ByteString,
+    ergoBoxCandidate'ergoTree :: !ByteString,
     -- | Secondary tokens the box contains
-    tokens :: ![Token],
+    ergoBoxCandidate'tokens :: ![Token],
     -- | Additional registers the box can carry over
-    additionalRegisters :: !ByteString,
+    ergoBoxCandidate'additionalRegisters :: !ByteString,
     -- | Height when a transaction containing the box was created.
     -- This height is declared by user and should not exceed height of the block,
     -- containing the transaction with this box.
-    creationHeight :: Word32
+    ergoBoxCandidate'creationHeight :: !Word32
   }
   deriving (Show, Read, Eq)
 
