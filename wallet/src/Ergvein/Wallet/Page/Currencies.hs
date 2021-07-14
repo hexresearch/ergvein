c5c4aa76a5c3db9a667c26cdc9d0041fbfddb2c0
fastpok <krutkinvs@gmail.com>
2021-07-14 21:13:28 +0300

    Formatting

--- wallet/src/Ergvein/Wallet/Page/Currencies.hs
+++ wallet/src/Ergvein/Wallet/Page/Currencies.hs
@@ -27,16 +27,16 @@ selectCurrenciesPage wt mnemonic = wrapperSimple True $ do
 #else
       retractableNext = setupPasswordPage wt Nothing mnemonic ac Nothing
 #endif
-    , retractablePrev = Just $ pure $ selectCurrenciesPage wt mnemonic -- TODO: uncomment this when ERGO is ready
+    , retractablePrev = Just $ pure $ selectCurrenciesPage wt mnemonic
     }
 
 selectCurrenciesWidget :: MonadFrontBase t m => [Currency] -> m (Event t [Currency])
 selectCurrenciesWidget currs = mdo
   divClass "select-currencies-title" $ h4 $ localizedText CurTitle
   eL <- traverse (\(cur,_) -> divClass "currency-toggle" $ do
-    let curD = fmap (toggled . snd .  (fromMaybe (cur, False)) . (find (\(c,_) -> c == cur))) curListD
+    let curD = fmap (toggled . snd .  fromMaybe (cur, False) . find (\(c,_) -> c == cur)) curListD
     e <- divButton curD (text $ showt cur)
-    pure (cur <$ e) ) $ startList
+    pure (cur <$ e)) startList
   curListD <- holdDyn startList $ poke (leftmost eL) $ \cur -> do
     curListS <- sampleDyn curListD
     pure $ fmap (invert cur) curListS
@@ -44,10 +44,10 @@ selectCurrenciesWidget currs = mdo
   btnE <- divButton curButtonD $ localizedText CurOk
   curListE <- performEvent $ ffor btnE $ \_ -> do
     curList <- sampleDyn curListD
-    pure $ fst . unzip $ filter (\(_,b) -> b == True) curList
+    pure $ map fst $ filter snd curList
   pure $ gate (current (fmap (enabledGate . checkTrue) curListD)) curListE
   where
-    checkTrue = (find (\(_,b) -> b == True))
+    checkTrue = find snd
 
     invert a (b,c) = if b == a
       then (b, not c)
@@ -65,6 +65,6 @@ selectCurrenciesWidget currs = mdo
       Just _ -> True
       Nothing -> False
 
-    startList = ffor allCurrencies $ \cur -> if (elem cur currs)
+    startList = ffor allCurrencies $ \cur -> if cur `elem` currs
       then (cur,True)
       else (cur,False)
