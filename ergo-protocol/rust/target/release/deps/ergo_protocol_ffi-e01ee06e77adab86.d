a886a35a780934f2ec18e7058d790dda52d8898d
fastpok <krutkinvs@gmail.com>
2021-11-12 19:21:07 +0300

    Removed unused files

--- ergo-protocol/rust/target/release/deps/ergo_protocol_ffi-e01ee06e77adab86.d
+++
@@ -1,5 +0,0 @@
-/home/vladimir/github-repos/ergvein/ergo-protocol/rust/target/release/deps/libergo_protocol_ffi-e01ee06e77adab86.a: src/lib.rs
-
-/home/vladimir/github-repos/ergvein/ergo-protocol/rust/target/release/deps/ergo_protocol_ffi-e01ee06e77adab86.d: src/lib.rs
-
-src/lib.rs:
--- ergo-protocol/rust/target/release/libergo_protocol_ffi.d
+++
@@ -1,1 +0,0 @@
-/home/vladimir/github-repos/ergvein/ergo-protocol/rust/target/release/libergo_protocol_ffi.a: /home/vladimir/github-repos/ergvein/ergo-protocol/rust/src/lib.rs
