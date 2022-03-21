2d4a9751698dff0c9cff35ff60354e0958b07887
Anton Gushcha <ncrashed@protonmail.com>
2022-03-21 22:27:12 +0300

    Upload modified reflex-platform for android 6 build

--- reflex-platform.nix
+++ reflex-platform.nix
@@ -1,8 +1,8 @@
 # Pin version of nixpkgs to ensure build reproduceability
-# import ((import <nixpkgs> {}).fetchFromGitHub {
-#  owner = "hexresearch";
-#  repo = "reflex-platform";
-#  rev = "67a9563255fdce351ec0aea5c438449e340cdc43";
-#  sha256  = "sha256-phqf6ulM1k3OQ8A8owUWaUjcKmXkLO9W39YkqgtSrjA=";
-# })
-import ../reflex-platform
+import ((import <nixpkgs> {}).fetchFromGitHub {
+ owner = "hexresearch";
+ repo = "reflex-platform";
+ rev = "4090ab1ab75f3cae90eecd36e3780b6aecef9580";
+ sha256  = "sha256-Sh8uSuVRFvVH8ig+A9OUdvrOZ4SqKnocaW4Uf5uxU2E=";
+})
+# import ../reflex-platform
