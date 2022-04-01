d9cf39613b70bf33fea5cd3fda5a0e7f23aa2982
fastpok <krutkinvs@gmail.com>
2022-04-01 17:43:53 +0300

    Fixed keytool error

--- release/create_keystore.sh
+++ release/create_keystore.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env nix-shell
-#!nix-shell -i bash -p androidenv.androidPkgs_9_0.platform-tools
+#!nix-shell -i bash -p adoptopenjdk-bin
 keytool -genkey -v -keystore ${1:-my_store.keystore} -alias ergvein_releasekey -keyalg RSA -keysize 4096 -validity 10000
